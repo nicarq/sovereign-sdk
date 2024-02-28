bbad07dcfa4ffb810b7e0ca543e14ad77f75a541
Blazej Kolad <blazejkolad@gmail.com>
2024-02-28 18:42:08 +0100

    Change CodeCommitment to Vec<u8> (#214)

--- full-node/sov-stf-runner/tests/helpers/mod.rs
+++ full-node/sov-stf-runner/tests/helpers/mod.rs
@@ -1,7 +1,2 @@
-use sov_rollup_interface::zk::aggregated_proof::CodeCommitment;
-
 pub mod hash_stf;
 pub mod runner_init;
-
-/// Code commitment for testing.
-pub const TEST_CODE_COMMITMENT: CodeCommitment = CodeCommitment([0; 32]);
