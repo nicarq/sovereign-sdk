0f73d01b314ea4c7e957b0adf99d5ab45cb372e2
Blazej Kolad <blazejkolad@gmail.com>
2024-03-25 13:22:43 +0100

    Implement feature gating for `mock-zkvm`. (#330)

---
+++ adapters/mock-zkvm/src/guest.rs
@@ -0,0 +1,16 @@
+use serde::Serialize;
+
+use crate::MockZkVerifier;
+/// A mock implementing the Guest.
+pub struct MockZkGuest {}
+
+impl sov_rollup_interface::zk::ZkvmGuest for MockZkGuest {
+    type Verifier = MockZkVerifier;
+    fn read_from_host<T: serde::de::DeserializeOwned>(&self) -> T {
+        unimplemented!()
+    }
+
+    fn commit<T: Serialize>(&self, _item: &T) {
+        unimplemented!()
+    }
+}
