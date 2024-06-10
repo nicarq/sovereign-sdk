ffc372d08c1c78247c218f5742c432360054430a
Preston Evans <32944016+preston-evans98@users.noreply.github.com>
2024-06-10 09:54:51 -0700

    Move attestation test to framework (#785)
    * Move attestation test to framework
    
    * clippy

--- module-system/module-implementations/sov-attester-incentives/src/tests/attestation_processing.rs
+++ module-system/module-implementations/sov-attester-incentives/src/tests/attestation_processing.rs
@@ -5,9 +5,9 @@ use sov_chain_state::ChainState;
 use sov_mock_da::MockDaSpec;
 use sov_modules_api::hooks::TxHooks;
 use sov_modules_api::optimistic::Attestation;
-use sov_modules_api::{Context, CryptoSpec, GasMeter, Module, PrivateKey, Spec, WorkingSet};
-use sov_prover_storage_manager::SimpleStorageManager;
-use sov_state::{Storage, StorageProof};
+use sov_modules_api::{CryptoSpec, GasMeter, Module, PrivateKey, Spec, WorkingSet};
+use sov_state::jmt::RootHash;
+use sov_state::{BorshCodec, SlotValue, Storage, StorageProof, StorageRoot};
 use sov_test_utils::generate_optimistic_runtime;
 use sov_test_utils::runtime::genesis::HighLevelOptimisticGenesisConfig;
 use sov_test_utils::runtime::sov_attester_incentives::{
@@ -17,10 +17,7 @@ use sov_test_utils::runtime::{
     run_test_with_setup_fn, MessageType, SlotTestCase, TxOutcome, TxTestCase,
 };
 
-use crate::call::AttesterIncentiveErrors;
-use crate::tests::helpers::{
-    commit_get_new_storage, setup, ExecutionSimulationVars, BOND_AMOUNT, INIT_HEIGHT,
-};
+use crate::tests::helpers::BOND_AMOUNT;
 type S = sov_test_utils::TestSpec;
 const DUMMY_CALL_MESSAGE: CallMessage<S, MockDaSpec> = CallMessage::UnbondChallenger; // This will get overwritten by the setup hook
 
@@ -221,228 +218,180 @@ fn test_process_valid_attestation() {
 
 #[test]
 fn test_burn_on_invalid_attestation() {
-    let tmpdir = tempfile::tempdir().unwrap();
-    let mut storage_manager = SimpleStorageManager::new(tmpdir.path());
-    let storage = storage_manager.create_storage();
-    let state = WorkingSet::new(storage.clone());
-    let (module, attester_address, _, sequencer, mut state) = setup(state);
+    generate_optimistic_runtime!(AttesterRuntime <=);
+    type RT = AttesterRuntime<S, MockDaSpec>;
+    // Generate a genesis config, then overwrite the attester key/address with ones that
+    // we know. We leave the other values untouched.
+    let mut genesis_config = HighLevelOptimisticGenesisConfig::generate();
+    let attester = &mut genesis_config.initial_attester;
+    let attester_key = <<S as Spec>::CryptoSpec as CryptoSpec>::PrivateKey::generate();
+    attester.address = <S as Spec>::Address::from(&attester_key.pub_key());
+    attester.bond = BOND_AMOUNT;
+    let attester_address = attester.address;
 
-    // Assert that the prover has the correct bond amount before processing the proof
-    assert_eq!(
-        module
-            .get_bond_amount(attester_address, crate::call::Role::Attester, &mut state)
-            .value,
-        BOND_AMOUNT
-    );
+    // Run genesis registering the attester and sequencer we've generated.
+    let genesis = GenesisConfig::from_minimal_config(genesis_config.into());
+    let mut round = 0;
+    let mut last_attested_slot = 0;
 
-    // Simulate the execution of a chain, with the genesis hash and two transitions after.
-    // Update the chain_state module and the optimistic module accordingly
-    let state_checkpoint = state.checkpoint().0;
-    commit_get_new_storage(storage, state_checkpoint, &mut storage_manager);
-    let (mut exec_vars, state_checkpoint) = ExecutionSimulationVars::execute(
-        3,
-        &module,
-        &mut storage_manager,
-        &sequencer,
-        &attester_address,
-    );
+    // Create a transaction setup function which overwrites dummy CallMessages with a valid attestation.
+    // We have to use the test frawework's setup hook to do this, since we don't know the correct state
+    // roots in advance.
+    let mut attestation_setup =
+        move |message: &mut <AttesterIncentives<S, MockDaSpec> as Module>::CallMessage,
+              _root: <<S as Spec>::Storage as Storage>::Root,
+              state: &mut <AttesterRuntime<S, MockDaSpec> as TxHooks>::TxState| {
+            if message == &DUMMY_CALL_MESSAGE {
+                let next_slot = last_attested_slot + 1;
+                let mut attestation = create_attestation(next_slot, &attester_address, state);
 
-    let transition_2 = exec_vars.pop().unwrap();
-    let transition_1 = exec_vars.pop().unwrap();
-    let initial_transition = exec_vars.pop().unwrap();
+                match round {
+                    0 => {
+                        // Process an invalid proof for genesis: everything is correct except the storage proof.
+                        // Must simply return an error. Cannot burn the token at this point because we don't know if the
+                        // sender is bonded or not.
+                        attestation.proof_of_bond.proof.value =
+                            Some(SlotValue::new(&(BOND_AMOUNT * 5), &BorshCodec));
+                    }
+                    1 => last_attested_slot += 1, // Since this attestation is unmodified, it will succeed so we need to move attesting to the next slot
+                    2 => {
+                        // Here we'll process an attestation with the wrong initial state root
+                        attestation.initial_state_root =
+                            StorageRoot::new(RootHash([255; 32]), RootHash([255; 32]));
+                    }
+                    3 => {
+                        // Here we'll process an attestation with the wrong post state root
+                        attestation.post_state_root =
+                            StorageRoot::new(RootHash([255; 32]), RootHash([255; 32]));
+                    }
+                    _ => unreachable!(),
+                };
 
-    let context = Context::<S>::new(attester_address, Default::default(), sequencer, 1);
-
-    let mut state = state_checkpoint.to_working_set_unmetered();
-    // Process an invalid proof for genesis: everything is correct except the storage proof.
-    // Must simply return an error. Cannot burn the token at this point because we don't know if the
-    // sender is bonded or not.
-    {
-        let attestation = Attestation {
-            initial_state_root: initial_transition.state_root,
-            slot_hash: [1; 32].into(),
-            post_state_root: transition_1.state_root,
-            proof_of_bond: sov_modules_api::optimistic::ProofOfBond {
-                claimed_transition_num: INIT_HEIGHT + 1,
-                proof: transition_1.state_proof.clone(),
-            },
+                *message =
+                    CallMessage::ProcessAttestation(WrappedAttestation { inner: attestation });
+                round += 1;
+            }
         };
 
-        let attestation_error = module
-            .process_attestation(&context, attestation.into(), &mut state)
-            .unwrap_err();
-
-        // The working set does not produce events because the method has returned an error
-        assert_eq!(state.events().len(), 0);
-
-        assert_eq!(
-            attestation_error,
-            AttesterIncentiveErrors::InvalidBondingProof,
-            "The bonding proof should fail"
-        );
-    }
-
-    // Assert that the prover's bond amount has not been burned
-    assert_eq!(
-        module
-            .get_bond_amount(attester_address, crate::call::Role::Attester, &mut state)
-            .value,
-        BOND_AMOUNT
-    );
-
-    // Now process a valid attestation for genesis.
-    {
-        let attestation = Attestation {
-            initial_state_root: initial_transition.state_root,
-            slot_hash: [1; 32].into(),
-            post_state_root: transition_1.state_root,
-            proof_of_bond: sov_modules_api::optimistic::ProofOfBond {
-                claimed_transition_num: INIT_HEIGHT + 1,
-                proof: initial_transition.state_proof,
+    run_test_with_setup_fn(
+        genesis.into_genesis_params(),
+        &mut attestation_setup,
+        vec![
+            // Run any empty slot, and check that the attester has the correct bond amount from genesis
+            SlotTestCase::<_, AttesterIncentives<S, MockDaSpec>, _> {
+                transaction_test_cases: vec![],
+                post_hook: Box::new(move |ws| {
+                    // Assert that genesis yielded the expected bond amount
+                    assert_eq!(
+                        AttesterIncentives::<S, MockDaSpec>::default()
+                            .bonded_attesters
+                            .get(&attester_address, ws)
+                            .unwrap_or_default(),
+                        BOND_AMOUNT,
+                    );
+                }),
             },
-        };
-
-        module
-            .process_attestation(&context, attestation.into(), &mut state)
-            .expect("An invalid proof is an error");
-
-        // The working set has only returned one event
-        assert_eq!(state.events().len(), 1);
-
-        // This is a valid attestation event.
-        let valid_event = state.take_event(0).unwrap();
-        let valid_event = valid_event.downcast::<crate::Event<S>>().unwrap();
-
-        assert_eq!(
-            valid_event,
-            crate::Event::ProcessedValidAttestation {
-                attester: attester_address
-            }
-        );
-    }
-
-    // Then process a new attestation having the wrong initial state root. The attester must be slashed, and the fees burnt
-    {
-        let attestation = Attestation {
-            initial_state_root: initial_transition.state_root,
-            slot_hash: [2; 32].into(),
-            post_state_root: transition_2.state_root,
-            proof_of_bond: sov_modules_api::optimistic::ProofOfBond {
-                claimed_transition_num: INIT_HEIGHT + 2,
-                proof: transition_1.state_proof.clone(),
+            // Run an empty slot
+            SlotTestCase::empty(),
+            SlotTestCase {
+                transaction_test_cases: vec![TxTestCase {
+                    outcome: TxOutcome::Reverted, // Fails without slashing because the bond proof was invalid
+                    message: MessageType::Plain(DUMMY_CALL_MESSAGE.clone(), attester_key.clone()),
+                }],
+                post_hook: Box::new(move |state| {
+                    // Assert that the attester was not slashed
+                    assert_eq!(
+                        AttesterIncentives::<S, MockDaSpec>::default()
+                            .get_bond_amount(attester_address, Role::Attester, state)
+                            .value,
+                        BOND_AMOUNT,
+                    );
+                }),
             },
-        };
-
-        module
-            .process_attestation(&context, attestation.into(), &mut state)
-            .expect("Since we slash the user we must exit gracefully");
-
-        // The working set has only returned one event
-        assert_eq!(state.events().len(), 1);
-
-        let slash_event = state.take_event(0).unwrap();
-        let slash_event = slash_event.downcast::<crate::Event<S>>().unwrap();
-
-        assert_eq!(
-            slash_event,
-            crate::Event::UserSlashed {
-                address: attester_address,
-                reason: crate::call::SlashingReason::InvalidInitialHash
-            }
-        );
-    }
-
-    // Check that the attester's bond has been burnt
-    assert_eq!(
-        module
-            .get_bond_amount(attester_address, crate::call::Role::Attester, &mut state)
-            .value,
-        0
-    );
-
-    // Check that the attestation is not part of the challengeable set
-    assert!(
-        module
-            .bad_transition_pool
-            .get(&(INIT_HEIGHT + 2), &mut state)
-            .is_none(),
-        "The transition should not exist in the pool"
-    );
-
-    // Bond the attester once more
-    module
-        .bond_user_helper(
-            BOND_AMOUNT,
-            &attester_address,
-            crate::call::Role::Attester,
-            &mut state,
-        )
-        .unwrap();
-
-    {
-        // Check that the attester has been bonded again
-
-        // The working set has only returned one event
-        assert_eq!(state.events().len(), 1);
-
-        let bond_event = state.take_event(0).unwrap();
-        let bond_event = bond_event.downcast::<crate::Event<S>>().unwrap();
-
-        assert_eq!(
-            bond_event,
-            crate::Event::BondedAttester {
-                new_deposit: BOND_AMOUNT,
-                total_bond: BOND_AMOUNT
-            }
-        );
-    }
-
-    // Process an attestation that has the right bonding proof and initial hash but has a faulty post transition hash.
-    {
-        let attestation = Attestation {
-            initial_state_root: transition_1.state_root,
-            slot_hash: [2; 32].into(),
-            post_state_root: transition_1.state_root,
-            proof_of_bond: sov_modules_api::optimistic::ProofOfBond {
-                claimed_transition_num: INIT_HEIGHT + 2,
-                proof: transition_1.state_proof,
-            },
-        };
-
-        module
-            .process_attestation(&context, attestation.into(), &mut state)
-            .expect("Since we slash the user we must exit gracefully");
-
-        // The working set has only returned one event
-        assert_eq!(state.events().len(), 1);
-
-        let slash_event = state.take_event(0).unwrap();
-        let slash_event = slash_event.downcast::<crate::Event<S>>().unwrap();
-
-        assert_eq!(
-            slash_event,
-            crate::Event::UserSlashed {
-                address: attester_address,
-                reason: crate::call::SlashingReason::TransitionInvalid
-            }
-        );
-    }
-
-    // Check that the attester's bond has been burnt
-    assert_eq!(
-        module
-            .get_bond_amount(attester_address, crate::call::Role::Attester, &mut state)
-            .value,
-        0
-    );
-
-    // The attestation should be part of the challengeable set and its associated value should be the BOND_AMOUNT
-    assert_eq!(
-        module
-            .bad_transition_pool
-            .get(&(INIT_HEIGHT + 2), &mut state)
-            .unwrap(),
-        BOND_AMOUNT,
-        "The transition should not exist in the pool"
+            SlotTestCase::from_txs(vec![
+                TxTestCase {
+                    outcome: TxOutcome::<RT>::Applied(Box::new(|state| {
+                        // Check that the attestation succeeded
+                        assert!(state.events().iter().any(|event| matches!(
+                            event.downcast_ref::<Event<S>>(),
+                            Some(Event::ProcessedValidAttestation { .. })
+                        )));
+                    })),
+                    message: MessageType::Plain(DUMMY_CALL_MESSAGE.clone(), attester_key.clone()),
+                },
+                TxTestCase {
+                    outcome: TxOutcome::<RT>::Applied(Box::new(move |state| {
+                        // Check that the attestation resulted in slashing
+                        assert!(state.events().iter().any(|event| matches!(
+                            event.downcast_ref::<Event<S>>(),
+                            Some(Event::UserSlashed { .. })
+                        )));
+                        // Assert that the attester was slashed
+                        assert_eq!(
+                            AttesterIncentives::<S, MockDaSpec>::default()
+                                .get_bond_amount(attester_address, Role::Attester, state)
+                                .value,
+                            0,
+                        );
+                        // Check that the invalid attestation is not part of the challengeable set.
+                        // (Since it has the wrong pre-state, no one will be fooled by it so we don't reward challengers)
+                        assert!(
+                            AttesterIncentives::<S, MockDaSpec>::default()
+                                .bad_transition_pool
+                                .get(&2, state)
+                                .is_none(),
+                            "The transition should not exist in the pool"
+                        );
+                    })),
+                    message: MessageType::Plain(DUMMY_CALL_MESSAGE.clone(), attester_key.clone()),
+                },
+            ]),
+            SlotTestCase::from_txs(vec![
+                TxTestCase {
+                    outcome: TxOutcome::<RT>::Applied(Box::new(move |state| {
+                        assert!(state.events().iter().any(|event| matches!(
+                            event.downcast_ref::<Event<S>>(),
+                            Some(Event::BondedAttester { .. })
+                        )));
+                        assert_eq!(
+                            AttesterIncentives::<S, MockDaSpec>::default()
+                                .get_bond_amount(attester_address, Role::Attester, state)
+                                .value,
+                            BOND_AMOUNT,
+                        );
+                    })),
+                    message: MessageType::Plain(
+                        CallMessage::BondAttester(BOND_AMOUNT),
+                        attester_key.clone(),
+                    ),
+                },
+                TxTestCase {
+                    outcome: TxOutcome::<RT>::Applied(Box::new(move |state| {
+                        // Check that the attestation resulted in slashing
+                        assert!(state.events().iter().any(|event| matches!(
+                            event.downcast_ref::<Event<S>>(),
+                            Some(Event::UserSlashed { .. })
+                        )));
+                        // Assert that the attester was slashed
+                        assert_eq!(
+                            AttesterIncentives::<S, MockDaSpec>::default()
+                                .get_bond_amount(attester_address, Role::Attester, state)
+                                .value,
+                            0,
+                        );
+                        // The attestation should be part of the challengeable set and its associated value should be the BOND_AMOUNT
+                        assert_eq!(
+                            AttesterIncentives::<S, MockDaSpec>::default()
+                                .bad_transition_pool
+                                .get(&2, state),
+                            Some(BOND_AMOUNT),
+                            "The transition should not exist in the pool"
+                        );
+                    })),
+                    message: MessageType::Plain(DUMMY_CALL_MESSAGE.clone(), attester_key.clone()),
+                },
+            ]),
+        ],
+        AttesterRuntime::default(),
     );
 }
