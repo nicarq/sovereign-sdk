8701cc0fa8fb3dd9aa4503cc681114250b808c2d
Nikolai Golub <nikolai@sovlabs.io>
2024-07-31 14:03:53 +0200

    Add toxiproxy to docker-compose (#1084)
    * Adding toxiproxy
    
    * Increase toxicity
    
    * Accidental reformatting

---
+++ docker/toxiproxy/configure.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Configuring toxiproxy for sequencer-0:\n"
+#TOXIPROXY_HOST="localhost"
+TOXIPROXY_HOST="toxiproxy"
+
+curl -s -XPOST -d '{"name" : "sequencer-0", "listen" : "0.0.0.0:26659", "upstream" : "sequencer-0:26658"}' http://$TOXIPROXY_HOST:8474/proxies
+#curl -s -XPOST -d '{"type" : "latency", "attributes" : {"latency" : 2000}}' http://localhost:8474/proxies/sequencer-0/toxics
+curl -s -XPOST -d '{"type": "timeout", "toxicity": 0.8, "attributes": {"timeout": 30000}}' http://$TOXIPROXY_HOST:8474/proxies/sequencer-0/toxics
+
+# Other options to consider
+# latency
+# slow_close
+# reset_peer
+
+echo "\n\n=====\nConfiguration is completed!"
