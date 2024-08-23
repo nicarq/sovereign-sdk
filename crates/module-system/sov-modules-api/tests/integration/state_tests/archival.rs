50f6a2721893fc2e980d2d774225d51332fcf665
theochap <80177219+theochap@users.noreply.github.com>
2024-08-23 11:17:05 +0200

    (Sov-modules-api): refactor state tests and add archival query tests (#1249)
    * Fix transaction details and add some tests to the framework
    
    * Refactoring more tests
    
    * Improve modularity of genesis config methods
    
    * Adding tests
    
    * Refactor capability tests
    
    * Refactoring state tests from sov modules api
    
    * Refactor archival query tests inside bank module
    
    * Reverting change

---
+++ crates/module-system/sov-modules-api/tests/integration/state_tests/archival.rs
@@ -0,0 +1,58 @@
+use std::convert::Infallible;
+
+use sov_modules_api::{ApiStateAccessor, StateCheckpoint, StateValue};
+use sov_state::Prefix;
+use sov_test_utils::storage::SimpleStorageManager;
+use unwrap_infallible::UnwrapInfallible;
+
+use crate::state_tests::*;
+
+fn increase_value_and_commit(
+    state_value: &StateValue<u32>,
+    storage: ProverStorage<StorageSpec>,
+    storage_manager: &mut SimpleStorageManager<StorageSpec>,
+) -> ProverStorage<StorageSpec> {
+    let mut state: StateCheckpoint<S> = StateCheckpoint::new(storage.clone());
+
+    let value = state_value.get(&mut state).unwrap_infallible().unwrap_or(0);
+
+    state_value
+        .set(&(value + 1), &mut state)
+        .unwrap_infallible();
+
+    commit_to_storage(state, storage, storage_manager)
+}
+
+/// Tests that the archival state is correctly retrieved from the DB and updates to the head state don't interfere
+#[test]
+fn archival_state_updates_correctly() -> Result<(), Infallible> {
+    let tmpdir = tempfile::tempdir().unwrap();
+    let mut storage_manager = SimpleStorageManager::<StorageSpec>::new(tmpdir.path());
+    let mut storage = storage_manager.create_storage();
+
+    let state_value = StateValue::new(Prefix::new(vec![0]));
+
+    for i in 1..100 {
+        let api_accessor = ApiStateAccessor::<S>::new(storage.clone());
+
+        for j in 1..(i - 1) {
+            let mut archival_api_accessor = api_accessor.get_archival_at(j);
+
+            let value = state_value.get(&mut archival_api_accessor)?;
+
+            assert_eq!(value, Some(j as u32));
+        }
+
+        storage = increase_value_and_commit(&state_value, storage, &mut storage_manager);
+
+        for j in 1..i {
+            let mut archival_api_accessor = api_accessor.get_archival_at(j);
+
+            let value = state_value.get(&mut archival_api_accessor)?;
+
+            assert_eq!(value, Some(j as u32));
+        }
+    }
+
+    Ok(())
+}
