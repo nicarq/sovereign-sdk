f8f80ce2fe96f572e8c9acd34be421b5708c53cd
theochap <80177219+theochap@users.noreply.github.com>
2024-08-23 12:36:07 +0200

    (Bank test refactor): refactor archival query tests (already reviewed just needs approval) (#1253)
    * Fix transaction details and add some tests to the framework
    
    * Refactoring more tests
    
    * Improve modularity of genesis config methods
    
    * Adding tests
    
    * Refactor capability tests
    
    * Refactoring state tests from sov modules api
    
    * Refactor archival query tests inside bank module

--- crates/module-system/module-implementations/sov-bank/tests/integration/archival_query_test.rs
+++ crates/module-system/module-implementations/sov-bank/tests/integration/archival_query_test.rs
@@ -1,355 +1,67 @@
-use sov_bank::{Amount, Bank, Coins, TokenId};
+use sov_bank::{Bank, Coins};
 use sov_modules_api::prelude::UnwrapInfallible;
-use sov_modules_api::{
-    ApiStateAccessor, InfallibleStateAccessor, Module, Spec, StateCheckpoint, StateReader,
-    StateWriter,
-};
-use sov_state::namespaces::Accessory;
-use sov_state::storage::{SlotKey, SlotValue, StateUpdate};
-use sov_state::{ProverStorage, Storage};
-use sov_test_utils::storage::SimpleStorageManager;
-use sov_test_utils::{TestStorageSpec as StorageSpec, TEST_DEFAULT_USER_BALANCE};
+use sov_test_utils::{AsUser, TestSpec};
 
 use crate::helpers::*;
 
-type S = sov_test_utils::TestSpec;
-
+/// Tests the archival query functionality of the bank module by doing some transfers and querying the old balances afterwards.
 #[test]
-fn transfer_initial_token() -> anyhow::Result<()> {
-    let initial_balance = TEST_DEFAULT_USER_BALANCE;
-    // The amount per transfer for the non-archival, most recent state.
+fn transfer_token_and_query_old_balances() {
+    let (
+        TestData {
+            token_name,
+            token_id,
+            user_high_token_balance: sender,
+            user_no_token_balance: receiver,
+            ..
+        },
+        mut runner,
+    ) = setup();
+
+    let sender_initial_token_balance = sender.token_balance(&token_name).unwrap();
+
     const AMOUNT_PER_TRANSFER: u64 = 10;
-    // The amount per transfer for the archival state.
-    const AMOUNT_PER_ARCHIVAL_TRANSFER: u64 = AMOUNT_PER_TRANSFER / 2;
-    // Another amount per transfer for a different archival state.
-    const AMOUNT_PER_ARCHIVAL_TRANSFER_2: u64 = AMOUNT_PER_TRANSFER / 3;
 
-    let bank_config = create_bank_config_with_token(4, initial_balance);
-    let tmpdir = tempfile::tempdir().unwrap();
-    let mut storage_manager = SimpleStorageManager::new(tmpdir.path());
-    let prover_storage = storage_manager.create_storage();
-    let state = StateCheckpoint::new(prover_storage.clone());
-    let bank = Bank::default();
-    let mut genesis_state = state.to_genesis_state_accessor::<Bank<S>>(&bank_config);
-    bank.genesis(&bank_config, &mut genesis_state).unwrap();
-    let mut state = genesis_state.checkpoint();
+    for i in 1..10 {
+        runner.execute(
+            sender.create_plain_message::<sov_bank::Bank<TestSpec>>(
+                sov_bank::CallMessage::Transfer {
+                    to: receiver.address(),
+                    coins: Coins {
+                        amount: AMOUNT_PER_TRANSFER,
+                        token_id,
+                    },
+                },
+            ),
+            None,
+        );
 
-    let token_id = sov_bank::GAS_TOKEN_ID;
-    let sender_address = bank_config.gas_token_config.address_and_balances[0].0;
-    let receiver_address = bank_config.gas_token_config.address_and_balances[1].0;
-    assert_ne!(sender_address, receiver_address);
+        for j in 1..i {
+            runner.query_state(|state| {
+                let archival_state = &mut state.get_archival_at(j);
+                assert_eq!(
+                    Bank::<TestSpec>::default()
+                        .get_balance_of(&sender.address(), token_id, archival_state)
+                        .unwrap_infallible(),
+                    Some(sender_initial_token_balance - AMOUNT_PER_TRANSFER * (j - 1))
+                );
 
-    let (sender_balance, receiver_balance) = query_sender_receiver_balances(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        &mut state,
-    );
-    assert_eq!(
-        (sender_balance, receiver_balance),
-        (TEST_DEFAULT_USER_BALANCE, TEST_DEFAULT_USER_BALANCE)
-    );
-    let prover_storage = commit(state, prover_storage, &mut storage_manager);
-    let mut state = StateCheckpoint::<S>::new(prover_storage.clone());
-
-    transfer(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        AMOUNT_PER_TRANSFER,
-        &mut state,
-    );
-
-    let (sender_balance, receiver_balance) = query_sender_receiver_balances(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        &mut state,
-    );
-    assert_eq!(
-        (sender_balance, receiver_balance),
-        (
-            TEST_DEFAULT_USER_BALANCE - AMOUNT_PER_TRANSFER,
-            TEST_DEFAULT_USER_BALANCE + AMOUNT_PER_TRANSFER
-        )
-    );
-
-    let prover_storage = commit(state, prover_storage, &mut storage_manager);
-
-    let mut state = StateCheckpoint::<S>::new(prover_storage.clone());
-
-    transfer(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        AMOUNT_PER_TRANSFER,
-        &mut state,
-    );
-
-    let (sender_balance, receiver_balance) = query_sender_receiver_balances(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        &mut state,
-    );
-    assert_eq!(
-        (sender_balance, receiver_balance),
-        (
-            TEST_DEFAULT_USER_BALANCE - 2 * AMOUNT_PER_TRANSFER,
-            TEST_DEFAULT_USER_BALANCE + 2 * AMOUNT_PER_TRANSFER
-        )
-    );
-    let prover_storage = commit(state, prover_storage, &mut storage_manager);
-
-    // Archival tests
-
-    let archival_slot: u64 = 2;
-    let state = &mut ApiStateAccessor::<S>::new(prover_storage.clone());
-    let archival = &mut state.get_archival_at(archival_slot);
-
-    let (sender_balance, receiver_balance) =
-        query_sender_receiver_balances(&bank, token_id, sender_address, receiver_address, archival);
-    assert_eq!(
-        (sender_balance, receiver_balance),
-        (
-            TEST_DEFAULT_USER_BALANCE - AMOUNT_PER_TRANSFER,
-            TEST_DEFAULT_USER_BALANCE + AMOUNT_PER_TRANSFER
-        )
-    );
-
-    // We want to transfer a different amount in the archival mode so that there is no collision with the `normal` transfers.
-    // modify in archival
-    transfer(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        AMOUNT_PER_ARCHIVAL_TRANSFER,
-        archival,
-    );
-
-    let (sender_balance, receiver_balance) =
-        query_sender_receiver_balances(&bank, token_id, sender_address, receiver_address, archival);
-    assert_eq!(
-        (sender_balance, receiver_balance),
-        (
-            TEST_DEFAULT_USER_BALANCE - AMOUNT_PER_TRANSFER - AMOUNT_PER_ARCHIVAL_TRANSFER,
-            TEST_DEFAULT_USER_BALANCE + AMOUNT_PER_TRANSFER + AMOUNT_PER_ARCHIVAL_TRANSFER
-        )
-    );
-
-    let archival_slot: u64 = 1;
-    let api_state = &mut ApiStateAccessor::<S>::new(prover_storage.clone());
-    let api_archival = &mut api_state.get_archival_at(archival_slot);
-    let (sender_balance, receiver_balance) = query_sender_receiver_balances(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        api_archival,
-    );
-    assert_eq!(
-        (sender_balance, receiver_balance),
-        (TEST_DEFAULT_USER_BALANCE, TEST_DEFAULT_USER_BALANCE)
-    );
-
-    // We want to transfer a different amount in the archival mode so that there is no collision with the `normal` transfers.
-    transfer(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        AMOUNT_PER_ARCHIVAL_TRANSFER_2,
-        api_archival,
-    );
-
-    let (sender_balance, receiver_balance) = query_sender_receiver_balances(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        api_archival,
-    );
-    assert_eq!(
-        (sender_balance, receiver_balance),
-        (
-            TEST_DEFAULT_USER_BALANCE - AMOUNT_PER_ARCHIVAL_TRANSFER_2,
-            TEST_DEFAULT_USER_BALANCE + AMOUNT_PER_ARCHIVAL_TRANSFER_2
-        )
-    );
-
-    let (sender_balance, receiver_balance) = query_sender_receiver_balances(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        api_state,
-    );
-    assert_eq!(
-        (sender_balance, receiver_balance),
-        (
-            TEST_DEFAULT_USER_BALANCE - 2 * AMOUNT_PER_TRANSFER,
-            TEST_DEFAULT_USER_BALANCE + 2 * AMOUNT_PER_TRANSFER
-        )
-    );
-
-    // Accessory tests
-    let mut state = StateCheckpoint::<S>::new(prover_storage.clone());
-    transfer(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        AMOUNT_PER_TRANSFER,
-        &mut state,
-    );
-
-    let (sender_balance, receiver_balance) = query_sender_receiver_balances(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        &mut state,
-    );
-    assert_eq!(
-        (sender_balance, receiver_balance),
-        (
-            TEST_DEFAULT_USER_BALANCE - 3 * AMOUNT_PER_TRANSFER,
-            TEST_DEFAULT_USER_BALANCE + 3 * AMOUNT_PER_TRANSFER
-        )
-    );
-
-    StateWriter::<Accessory>::set(
-        &mut state,
-        &SlotKey::from_slice(b"k"),
-        SlotValue::from(b"v1".to_vec()),
-    )?;
-
-    let prover_storage = commit(state, prover_storage, &mut storage_manager);
-
-    let api_state = &mut ApiStateAccessor::<S>::new(prover_storage.clone());
-    let val = StateReader::<Accessory>::get(api_state, &SlotKey::from_slice(b"k"))?.unwrap();
-    assert_eq!("v1", String::from_utf8(val.value().to_vec()).unwrap());
-
-    // next block
-
-    let mut state = StateCheckpoint::<S>::new(prover_storage.clone());
-    transfer(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        AMOUNT_PER_TRANSFER,
-        &mut state,
-    );
-
-    let (sender_balance, receiver_balance) = query_sender_receiver_balances(
-        &bank,
-        token_id,
-        sender_address,
-        receiver_address,
-        &mut state,
-    );
-    assert_eq!(
-        (sender_balance, receiver_balance),
-        (
-            TEST_DEFAULT_USER_BALANCE - 4 * AMOUNT_PER_TRANSFER,
-            TEST_DEFAULT_USER_BALANCE + 4 * AMOUNT_PER_TRANSFER
-        )
-    );
-    StateWriter::<Accessory>::set(
-        &mut state,
-        &SlotKey::from_slice(b"k"),
-        SlotValue::from(b"v2".to_vec()),
-    )?;
-
-    let prover_storage = commit(state, prover_storage, &mut storage_manager);
-
-    let api_state = &mut ApiStateAccessor::<S>::new(prover_storage.clone());
-    let val = StateReader::<Accessory>::get(api_state, &SlotKey::from_slice(b"k"))?.unwrap();
-    assert_eq!("v2", String::from_utf8(val.value().to_vec()).unwrap());
-
-    // archival versioned state query
-
-    let archival_slot = 3;
-    let mut state: ApiStateAccessor<S> = ApiStateAccessor::new(prover_storage.clone());
-    let mut archival = state.get_archival_at(archival_slot);
-    let val = StateReader::<Accessory>::get(&mut archival, &SlotKey::from_slice(b"k"))?.unwrap();
-    assert_eq!("v1", String::from_utf8(val.value().to_vec()).unwrap());
-
-    // archival accessory set
-
-    StateWriter::<Accessory>::set(
-        &mut archival,
-        &SlotKey::from_slice(b"k"),
-        SlotValue::from(b"v3".to_vec()),
-    )
-    .expect("Should be able to set state");
-    let val = StateReader::<Accessory>::get(&mut archival, &SlotKey::from_slice(b"k"))?.unwrap();
-    assert_eq!("v3", String::from_utf8(val.value().to_vec()).unwrap());
-
-    let val = StateReader::<Accessory>::get(&mut state, &SlotKey::from_slice(b"k"))?.unwrap();
-    assert_eq!("v2", String::from_utf8(val.value().to_vec()).unwrap());
-
-    Ok(())
-}
-
-fn query_sender_receiver_balances(
-    bank: &Bank<S>,
-    token_id: TokenId,
-    sender_address: <S as Spec>::Address,
-    receiver_address: <S as Spec>::Address,
-    state: &mut impl InfallibleStateAccessor,
-) -> (u64, u64) {
-    let sender_balance = bank
-        .get_balance_of(&sender_address, token_id, state)
-        .unwrap_infallible()
-        .unwrap();
-    let receiver_balance = bank
-        .get_balance_of(&receiver_address, token_id, state)
-        .unwrap_infallible()
-        .unwrap();
-    (sender_balance, receiver_balance)
-}
-
-fn transfer(
-    bank: &Bank<S>,
-    token_id: TokenId,
-    sender_address: <S as Spec>::Address,
-    receiver_address: <S as Spec>::Address,
-    transfer_amount: Amount,
-    state: &mut impl InfallibleStateAccessor,
-) {
-    let to = receiver_address;
-    let coin_amount = Coins {
-        amount: transfer_amount,
-        token_id,
-    };
-
-    bank.transfer_from(&sender_address, &to, coin_amount, state)
-        .unwrap();
-}
-
-fn commit(
-    state: StateCheckpoint<S>,
-    storage: ProverStorage<StorageSpec>,
-    storage_manager: &mut SimpleStorageManager<StorageSpec>,
-) -> ProverStorage<StorageSpec> {
-    // Save checkpoint
-    let (cache_log, accessory_delta, witness) = state.freeze();
-
-    let (_, mut state_update) = storage
-        .compute_state_update(cache_log, &witness)
-        .expect("JMT update must succeed");
-
-    state_update.add_accessory_items(accessory_delta.freeze());
-
-    let change_set = storage.materialize_changes(&state_update);
-    storage_manager.commit(change_set);
-    storage_manager.create_storage()
+                if j > 1 {
+                    assert_eq!(
+                        Bank::<TestSpec>::default()
+                            .get_balance_of(&receiver.address(), token_id, archival_state)
+                            .unwrap_infallible(),
+                        Some(AMOUNT_PER_TRANSFER * (j - 1))
+                    );
+                } else {
+                    assert_eq!(
+                        Bank::<TestSpec>::default()
+                            .get_balance_of(&receiver.address(), token_id, archival_state)
+                            .unwrap_infallible(),
+                        None
+                    );
+                }
+            });
+        }
+    }
 }
--- crates/module-system/module-implementations/sov-bank/tests/integration/helpers/mod.rs
+++ crates/module-system/module-implementations/sov-bank/tests/integration/helpers/mod.rs
@@ -1,42 +1,10 @@
-use sov_bank::{BankConfig, GasTokenConfig, TokenId};
-use sov_modules_api::test_utils::generate_address as gen_address_generic;
-use sov_modules_api::Spec;
+use sov_bank::TokenId;
 use sov_test_utils::runtime::genesis::optimistic::HighLevelOptimisticGenesisConfig;
 use sov_test_utils::runtime::genesis::TestTokenName;
 use sov_test_utils::runtime::TestRunner;
 use sov_test_utils::{generate_optimistic_runtime, MockDaSpec, TestUser};
 
 pub type S = sov_test_utils::TestSpec;
-
-// This code is not actually dead; rustc treats each test file as a separate crate
-// so this code looks unused during some of the compilations.
-pub fn generate_address(name: &str) -> <S as Spec>::Address {
-    gen_address_generic::<S>(name)
-}
-
-pub fn create_bank_config_with_token(
-    addresses_count: usize,
-    initial_balance: u64,
-) -> BankConfig<S> {
-    let address_and_balances = (0..addresses_count)
-        .map(|i| {
-            let key = format!("key_{}", i);
-            let addr = generate_address(&key);
-            (addr, initial_balance)
-        })
-        .collect();
-
-    let token_name = "InitialToken".to_owned();
-
-    BankConfig {
-        gas_token_config: GasTokenConfig {
-            token_name,
-            address_and_balances,
-            authorized_minters: vec![],
-        },
-        tokens: vec![],
-    }
-}
 
 generate_optimistic_runtime!(TestBankRuntime <=);
 
