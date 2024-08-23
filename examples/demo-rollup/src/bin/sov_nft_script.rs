0a5fbf98dd613b7daf62d47fdd6928fdc9fbdfd9
Nikolai Golub <nikolai@sovlabs.io>
2024-08-23 18:25:49 +0200

    Remove `sov-test-utils` dependency from sov-demo-rollup's nft script (#1265)

--- examples/demo-rollup/src/bin/sov_nft_script.rs
+++ examples/demo-rollup/src/bin/sov_nft_script.rs
@@ -2,17 +2,23 @@ use std::time::Duration;
 
 use demo_stf::runtime::RuntimeCall;
 use sov_mock_da::MockDaSpec;
+use sov_mock_zkvm::MockZkVerifier;
 use sov_modules_api::macros::config_value;
-use sov_modules_api::transaction::{Transaction, UnsignedTransaction};
+use sov_modules_api::transaction::{PriorityFeeBips, Transaction, UnsignedTransaction};
 use sov_modules_api::{PrivateKey, Spec};
 use sov_nft_module::utils::{
     get_collection_id, get_create_collection_message, get_mint_nft_message,
     get_transfer_nft_message,
 };
 use sov_nft_module::{CallMessage, CollectionId};
-use sov_test_utils::{
-    ApiClient, TestPrivateKey, TestSpec, TEST_DEFAULT_MAX_FEE, TEST_DEFAULT_MAX_PRIORITY_FEE,
-};
+use sov_rollup_interface::execution_mode::Native;
+use sov_rollup_interface::zk::CryptoSpec;
+
+const DEFAULT_MAX_FEE: u64 = 100_000_000;
+const DEFAULT_MAX_PRIORITY_FEE: PriorityFeeBips = PriorityFeeBips::from_percentage(0);
+type DemoSpec = sov_modules_api::default_spec::DefaultSpec<MockZkVerifier, MockZkVerifier, Native>;
+type DemoCryptoSpec = <DemoSpec as Spec>::CryptoSpec;
+type DemoPrivateKey = <DemoCryptoSpec as CryptoSpec>::PrivateKey;
 
 const COLLECTION_1: &str = "Sovereign Squirrel Syndicate";
 const COLLECTION_2: &str = "Celestial Dolphins";
@@ -21,17 +27,17 @@ const COLLECTION_3: &str = "Risky Rhinos";
 const DUMMY_URL: &str = "http://foobar.storage";
 
 pub fn build_transaction(
-    signer: &TestPrivateKey,
-    message: CallMessage<TestSpec>,
+    signer: &DemoPrivateKey,
+    message: CallMessage<DemoSpec>,
     nonce: u64,
-) -> Transaction<TestSpec> {
-    let runtime_encoded_message = RuntimeCall::<TestSpec, MockDaSpec>::nft(message);
+) -> Transaction<DemoSpec> {
+    let runtime_encoded_message = RuntimeCall::<DemoSpec, MockDaSpec>::nft(message);
     let chain_id = config_value!("CHAIN_ID");
-    let max_priority_fee_bips = TEST_DEFAULT_MAX_PRIORITY_FEE;
-    let max_fee = TEST_DEFAULT_MAX_FEE;
+    let max_priority_fee_bips = DEFAULT_MAX_PRIORITY_FEE;
+    let max_fee = DEFAULT_MAX_FEE;
     let gas_limit = None;
 
-    Transaction::<TestSpec>::new_signed_tx(
+    Transaction::<DemoSpec>::new_signed_tx(
         signer,
         UnsignedTransaction::new(
             borsh::to_vec(&runtime_encoded_message).unwrap(),
@@ -45,18 +51,18 @@ pub fn build_transaction(
 }
 
 pub fn build_create_collection_transactions(
-    creator_pk: &TestPrivateKey,
+    creator_pk: &DemoPrivateKey,
     start_nonce: &mut u64,
     base_uri: &str,
     collections: &[&str],
-) -> Vec<Transaction<TestSpec>> {
+) -> Vec<Transaction<DemoSpec>> {
     collections
         .iter()
         .map(|&collection_name| {
             let tx = build_transaction(
                 creator_pk,
                 get_create_collection_message(
-                    &creator_pk.to_address::<<TestSpec as Spec>::Address>(),
+                    &creator_pk.to_address::<<DemoSpec as Spec>::Address>(),
                     collection_name,
                     base_uri,
                 ),
@@ -70,24 +76,24 @@ pub fn build_create_collection_transactions(
 
 /// Convenience and readability wrapper for build_mint_nft_transaction
 pub fn build_mint_transactions(
-    creator_pk: &TestPrivateKey,
+    creator_pk: &DemoPrivateKey,
     start_nonce: &mut u64,
     collection: &str,
     start_nft_id: &mut u64,
     num: usize,
     base_uri: &str,
-    owner_pk: &TestPrivateKey,
-) -> Vec<Transaction<TestSpec>> {
+    owner_pk: &DemoPrivateKey,
+) -> Vec<Transaction<DemoSpec>> {
     (0..num)
         .map(|_| {
             let tx = build_transaction(
                 creator_pk,
                 get_mint_nft_message(
-                    &creator_pk.to_address::<<TestSpec as Spec>::Address>(),
+                    &creator_pk.to_address::<<DemoSpec as Spec>::Address>(),
                     collection,
                     *start_nft_id,
                     base_uri,
-                    &owner_pk.to_address::<<TestSpec as Spec>::Address>(),
+                    &owner_pk.to_address::<<DemoSpec as Spec>::Address>(),
                 ),
                 *start_nonce,
             );
@@ -99,15 +105,15 @@ pub fn build_mint_transactions(
 }
 
 pub fn build_transfer_transactions(
-    signer: &TestPrivateKey,
+    signer: &DemoPrivateKey,
     start_nonce: &mut u64,
     collection_id: &CollectionId,
     nft_ids: Vec<u64>,
-) -> Vec<Transaction<TestSpec>> {
+) -> Vec<Transaction<DemoSpec>> {
     nft_ids
         .into_iter()
         .map(|nft_id| {
-            let new_owner = TestPrivateKey::generate().to_address::<<TestSpec as Spec>::Address>();
+            let new_owner = DemoPrivateKey::generate().to_address::<<DemoSpec as Spec>::Address>();
             let tx = build_transaction(
                 signer,
                 get_transfer_nft_message(collection_id, nft_id, &new_owner),
@@ -121,21 +127,20 @@ pub fn build_transfer_transactions(
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let creator_pk = TestPrivateKey::generate();
-    let owner_1_pk = TestPrivateKey::generate();
-    let owner_2_pk = TestPrivateKey::generate();
+    let creator_pk = DemoPrivateKey::generate();
+    let owner_1_pk = DemoPrivateKey::generate();
+    let owner_2_pk = DemoPrivateKey::generate();
 
-    let rpc_port = 12345;
     let rest_port = 12346;
-    let client = ApiClient::new(rpc_port, rest_port).await?;
+    let sequencer_client =
+        sov_sequencer_json_client::Client::new(&format!("http://127.0.0.1:{rest_port}/sequencer"));
 
     let mut nonce = 0;
     let collections = [COLLECTION_1, COLLECTION_2, COLLECTION_3];
     let transactions =
         build_create_collection_transactions(&creator_pk, &mut nonce, DUMMY_URL, &collections);
 
-    client
-        .sequencer
+    sequencer_client
         .publish_batch_with_serialized_txs(&transactions)
         .await?;
 
@@ -175,18 +180,17 @@ async fn main() -> anyhow::Result<()> {
         &owner_1_pk,
     ));
 
-    client
-        .sequencer
+    sequencer_client
         .publish_batch_with_serialized_txs(&transactions)
         .await?;
 
     // TODO: remove after https://github.com/Sovereign-Labs/sovereign-sdk/issues/949 is fixed
     tokio::time::sleep(Duration::from_millis(3000)).await;
 
-    let collection_1_address = get_collection_id::<TestSpec>(
+    let collection_1_address = get_collection_id::<DemoSpec>(
         COLLECTION_1,
         creator_pk
-            .to_address::<<TestSpec as Spec>::Address>()
+            .to_address::<<DemoSpec as Spec>::Address>()
             .as_ref(),
     );
 
@@ -199,8 +203,7 @@ async fn main() -> anyhow::Result<()> {
         nft_ids_to_transfer,
     );
 
-    client
-        .sequencer
+    sequencer_client
         .publish_batch_with_serialized_txs(&transactions)
         .await?;
 
