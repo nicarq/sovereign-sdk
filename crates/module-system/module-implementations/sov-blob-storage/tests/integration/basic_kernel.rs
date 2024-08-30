ce6ba28983527525974e39ff557d080f1004da76
theochap <80177219+theochap@users.noreply.github.com>
2024-08-30 12:37:01 +0200

    Add soft-confirmation tests in blob-storage. (#1331)
    * Start refactoring
    
    * Refactoring blob storage tests
    
    * Adding custom kernel to testing framework
    
    * Starting to refactor capability tests
    
    * Adding kernel config
    
    * Adding comments
    
    * Adding soft confirmation tests
    
    * Adding soft confirmation tests
    
    * Remove is_preferred from sequencer struct
    
    * Lint
    
    * PR comments
    
    * Removing newtype
    
    * Nit
    
    * Fixing nit
    
    * Refactor in separate files
    
    * Refactor in separate files
    
    * Simplify soft confirmations
    
    * Lint
    
    * Docs
    
    * Simplify helpers
    
    * Simplify the blob-storage tests
    
    * Adding comments
    
    * Lints
    
    * Remove extra data structs
    
    * Simplify method
    
    * Fixing test
    
    * Lint
    
    * Move last slot receipt to blob storage tests

---
+++ crates/module-system/module-implementations/sov-blob-storage/tests/integration/basic_kernel.rs
@@ -0,0 +1,56 @@
+use sov_blob_storage::BlobStorage;
+
+use crate::helpers_basic_kernel::{
+    assert_blobs_are_correctly_received_basic_kernel, setup_basic_kernel,
+};
+use crate::{Da, TestData, S};
+
+#[test]
+fn empty_test() {
+    let (_, runner) = setup_basic_kernel();
+
+    runner.query_state(|state| {
+        assert!(BlobStorage::<S, Da>::default()
+            .take_blobs_for_slot_number(1, state)
+            .is_empty());
+    });
+}
+
+/// Tests that the blob storage module can store and retrieve blobs.
+/// The test creates a batch of blobs, and then checks that the blobs are stored and retrieved correctly by
+/// comparing the hashes of the blobs sent and the hashes of the receipts received.
+#[test]
+fn store_and_retrieve_standard_basic_kernel() {
+    let (
+        TestData {
+            preferred_sequencer,
+            ..
+        },
+        mut runner,
+    ) = setup_basic_kernel();
+
+    runner.query_state(|state| {
+        let blob_storage = BlobStorage::<S, Da>::default();
+
+        assert!(blob_storage.take_blobs_for_slot_number(1, state).is_empty());
+        assert!(blob_storage.take_blobs_for_slot_number(2, state).is_empty());
+        assert!(blob_storage.take_blobs_for_slot_number(3, state).is_empty());
+        assert!(blob_storage.take_blobs_for_slot_number(4, state).is_empty());
+    });
+
+    runner.advance_slots(1);
+
+    // Create three slots, each containing a batch of blobs.
+    // We should receive three receipts in the same order as the blobs were sent.
+    let slots = vec![
+        vec![preferred_sequencer.clone(); 3],
+        vec![preferred_sequencer.clone()],
+        vec![preferred_sequencer.clone()],
+    ];
+
+    assert_blobs_are_correctly_received_basic_kernel(
+        slots,
+        vec![vec![0, 1, 2], vec![3], vec![4]],
+        &mut runner,
+    );
+}
