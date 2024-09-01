ae3c7fd89b1128b22468f510abb40401d77277dc
theochap <80177219+theochap@users.noreply.github.com>
2024-09-01 09:40:16 +0200

    Move outdated methods out of the `sov-test-utils` to the `stf-tests` + cleanup lib file. (#1347)
    * Start refactoring
    
    * Refactoring blob storage tests
    
    * Adding custom kernel to testing framework
    
    * Starting to refactor capability tests
    
    * Adding kernel config
    
    * Adding comments
    
    * Adding soft confirmation tests
    
    * Adding soft confirmation tests
    
    * Remove is_preferred from sequencer struct
    
    * Lint
    
    * PR comments
    
    * Removing newtype
    
    * Nit
    
    * Fixing nit
    
    * Refactor in separate files
    
    * Refactor in separate files
    
    * Simplify soft confirmations
    
    * Lint
    
    * Docs
    
    * Simplify helpers
    
    * Simplify the blob-storage tests
    
    * Adding comments
    
    * Add blob processing tests
    
    * Lints
    
    * Add check for virtual slot increases
    
    * Adding test for slot adjustment
    
    * Remove extra data structs
    
    * Simplify method
    
    * Fixing test
    
    * Adding a couple of tests
    
    * Lint
    
    * Adding the last tests
    
    * Adding the last tests
    
    * Finishing unregistered tests
    
    * Remove old tests
    
    * Lint
    
    * Move last slot receipt to blob storage tests
    
    * Remove outdated methods from test utils
    
    * Move outdated methods out of test-utils
    
    * Move `generators.rs` to the `generators` module
    
    * Fix import
    
    * Export message generator

---
+++ crates/module-system/sov-test-utils/src/generators/mod.rs
@@ -0,0 +1,181 @@
+//! Several utilities needed to generate message for testing.
+//!
+//! TODO: Add a doctest to describe how to generate messages in tests.
+
+use std::rc::Rc;
+
+use sov_modules_api::capabilities::Authenticator;
+use sov_modules_api::macros::config_value;
+use sov_modules_api::transaction::{PriorityFeeBips, Transaction, TxDetails, UnsignedTransaction};
+use sov_modules_api::{Batch, CryptoSpec, EncodeCall, GasArray, Module, RawTx, Spec};
+
+use crate::{TEST_DEFAULT_GAS_LIMIT, TEST_DEFAULT_MAX_FEE, TEST_DEFAULT_MAX_PRIORITY_FEE};
+
+/// Utilities for generating messages for the bank module.
+pub mod bank;
+/// Utilities for generating messages for the sequencer registry module.
+pub mod sequencer_registry;
+/// Utilities for generating messages for the value setter module.
+pub mod value_setter;
+
+/// A generic message object used to create transactions.
+pub struct Message<S: Spec, Mod: Module> {
+    /// The sender's private key.
+    pub sender_key: Rc<<S::CryptoSpec as CryptoSpec>::PrivateKey>,
+    /// The message content.
+    pub content: Mod::CallMessage,
+    /// Data related to fees and gas handling.
+    pub details: TxDetails<S>,
+    /// The message nonce.
+    pub nonce: u64,
+}
+
+impl<S: Spec, Mod: Module> Message<S, Mod> {
+    fn new(
+        sender_key: Rc<<S::CryptoSpec as CryptoSpec>::PrivateKey>,
+        content: Mod::CallMessage,
+        chain_id: u64,
+        max_priority_fee_bips: PriorityFeeBips,
+        max_fee: u64,
+        gas_limit: Option<S::Gas>,
+        nonce: u64,
+    ) -> Self {
+        Self {
+            sender_key,
+            content,
+            details: TxDetails {
+                chain_id,
+                max_priority_fee_bips,
+                max_fee,
+                gas_limit,
+            },
+            nonce,
+        }
+    }
+
+    /// Converts a [`Message`] into a [`Transaction`] using the [`TxDetails`] provided by the [`Message`].
+    pub fn to_tx<Encoder: EncodeCall<Mod>>(self) -> sov_modules_api::transaction::Transaction<S> {
+        let message = Encoder::encode_call(self.content);
+        Transaction::<S>::new_signed_tx(
+            &self.sender_key,
+            UnsignedTransaction::new(
+                message,
+                self.details.chain_id,
+                self.details.max_priority_fee_bips,
+                self.details.max_fee,
+                self.nonce,
+                self.details.gas_limit,
+            ),
+        )
+    }
+}
+
+/// Trait used to generate messages from the DA layer to automate module testing
+pub trait MessageGenerator {
+    /// The default chain ID to use for the messages.
+    const DEFAULT_CHAIN_ID: u64 = config_value!("CHAIN_ID");
+
+    /// Module where the messages originate from.
+    type Module: Module;
+
+    /// Module spec
+    type Spec: Spec;
+
+    /// Generates a list of messages originating from the module using the provided transaction details.
+    fn create_messages(
+        &self,
+        chain_id: u64,
+        max_priority_fee_bips: PriorityFeeBips,
+        max_fee: u64,
+        estimated_gas_usage: Option<<Self::Spec as Spec>::Gas>,
+    ) -> Vec<Message<Self::Spec, Self::Module>>;
+
+    /// Generates a list of messages originating from the module using default transaction details.
+    /// Note: sets the gas usage to the default gas limit.
+    fn create_default_messages(&self) -> Vec<Message<Self::Spec, Self::Module>> {
+        self.create_messages(
+            Self::DEFAULT_CHAIN_ID,
+            TEST_DEFAULT_MAX_PRIORITY_FEE,
+            TEST_DEFAULT_MAX_FEE,
+            Some(<Self::Spec as Spec>::Gas::from_slice(
+                &TEST_DEFAULT_GAS_LIMIT,
+            )),
+        )
+    }
+
+    /// Generates a list of messages originating from the module using default transaction details and no gas usage.
+    fn create_default_messages_without_gas_usage(&self) -> Vec<Message<Self::Spec, Self::Module>> {
+        self.create_messages(
+            Self::DEFAULT_CHAIN_ID,
+            TEST_DEFAULT_MAX_PRIORITY_FEE,
+            TEST_DEFAULT_MAX_FEE,
+            None,
+        )
+    }
+
+    /// Creates a vector of raw transactions from the module.
+    fn create_default_raw_txs<Encoder: EncodeCall<Self::Module>, Auth: Authenticator>(
+        &self,
+    ) -> Vec<RawTx> {
+        self.create_raw_txs::<Encoder, Auth>(
+            Self::DEFAULT_CHAIN_ID,
+            TEST_DEFAULT_MAX_PRIORITY_FEE,
+            TEST_DEFAULT_MAX_FEE,
+            Some(<Self::Spec as Spec>::Gas::from_slice(
+                &TEST_DEFAULT_GAS_LIMIT,
+            )),
+        )
+    }
+
+    /// Generates a list of raw transactions originating from the module using default transaction details and no gas usage.
+    fn create_default_raw_txs_without_gas_usage<
+        Encoder: EncodeCall<Self::Module>,
+        Auth: Authenticator,
+    >(
+        &self,
+    ) -> Vec<RawTx> {
+        self.create_raw_txs::<Encoder, Auth>(
+            Self::DEFAULT_CHAIN_ID,
+            TEST_DEFAULT_MAX_PRIORITY_FEE,
+            TEST_DEFAULT_MAX_FEE,
+            None,
+        )
+    }
+
+    /// Creates a vector of raw transactions from the module.
+    fn create_raw_txs<Encoder: EncodeCall<Self::Module>, Auth: Authenticator>(
+        &self,
+        chain_id: u64,
+        max_priority_fee_bips: PriorityFeeBips,
+        max_fee: u64,
+        estimated_gas_usage: Option<<Self::Spec as Spec>::Gas>,
+    ) -> Vec<RawTx> {
+        let messages_iter = self
+            .create_messages(
+                chain_id,
+                max_priority_fee_bips,
+                max_fee,
+                estimated_gas_usage,
+            )
+            .into_iter();
+        let mut serialized_messages = Vec::default();
+        for message in messages_iter {
+            let tx = message.to_tx::<Encoder>();
+            serialized_messages.push(Auth::encode(borsh::to_vec(&tx).unwrap()).unwrap());
+        }
+        serialized_messages
+    }
+
+    /// Generates a list of blobs originating from the module using default transaction details.
+    /// This function calls [`MessageGenerator::create_default_raw_txs`] and then wraps the resulting vec of [`RawTx`]s into a [`Batch`].
+    fn create_blobs<Encoder: EncodeCall<Self::Module>, Auth: Authenticator>(&self) -> Vec<u8> {
+        let txs: Vec<RawTx> = self
+            .create_default_raw_txs::<Encoder, Auth>()
+            .into_iter()
+            .collect();
+
+        let batch = Batch::new(txs);
+
+        borsh::to_vec(&batch).unwrap()
+    }
+}
