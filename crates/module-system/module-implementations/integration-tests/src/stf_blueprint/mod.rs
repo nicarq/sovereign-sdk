8b952f97a8466b9d699d0b970eb9374b3a84b773
Blazej Kolad <blazejkolad@gmail.com>
2024-10-22 22:18:45 +0200

    Add integration test for the `stf-blueprint` part 2 (#1711)

--- crates/module-system/module-implementations/integration-tests/src/stf_blueprint/mod.rs
+++ crates/module-system/module-implementations/integration-tests/src/stf_blueprint/mod.rs
@@ -1,17 +1,18 @@
 mod registered;
 
 use sov_bank::Bank;
+use sov_modules_api::macros::config_value;
 use sov_modules_api::prelude::UnwrapInfallible;
-use sov_modules_api::{ApiStateAccessor, DaSpec, Spec};
-use sov_modules_stf_blueprint::TxProcessingError;
+use sov_modules_api::transaction::{PriorityFeeBips, UnsignedTransaction};
+use sov_modules_api::{ApiStateAccessor, DaSpec, EncodeCall, Spec};
 use sov_sequencer_registry::SequencerRegistry;
 use sov_test_utils::runtime::genesis::optimistic::HighLevelOptimisticGenesisConfig;
 use sov_test_utils::runtime::{config_gas_token_id, Payable, TestRunner};
 use sov_test_utils::{
-    generate_optimistic_runtime, TestSequencer, TestUser, TransactionTestCase,
+    generate_optimistic_runtime, TestSequencer, TestUser, TransactionType,
     TEST_DEFAULT_USER_BALANCE,
 };
-use sov_value_setter::ValueSetter;
+use sov_value_setter::{CallMessage, ValueSetter};
 
 type S = sov_test_utils::TestSpec;
 
@@ -61,4 +62,97 @@ pub(crate) fn setup() -> (
     let sequencer_account = genesis_config.initial_sequencer.clone();
 
     (runner, admin_account, sequencer_account)
+}
+
+fn create_tx_bad_chain_id(
+    nonce: u64,
+    max_priority_fee_bips: PriorityFeeBips,
+    signer: &TestUser<S>,
+) -> TransactionType<ValueSetter<S>, S> {
+    let encoded_message =
+        <IntegTestRuntime<S> as EncodeCall<ValueSetter<S>>>::encode_call(CallMessage::SetValue(8));
+
+    let utx = UnsignedTransaction::new(
+        encoded_message.clone(),
+        config_value!("CHAIN_ID") + 1,
+        max_priority_fee_bips,
+        200_000,
+        nonce,
+        None,
+    );
+
+    TransactionType::<ValueSetter<S>, S>::pre_signed(utx, signer.private_key())
+}
+
+fn create_tx_valid(
+    nonce: u64,
+    max_priority_fee_bips: PriorityFeeBips,
+    signer: &TestUser<S>,
+) -> TransactionType<ValueSetter<S>, S> {
+    let encoded_message =
+        <IntegTestRuntime<S> as EncodeCall<ValueSetter<S>>>::encode_call(CallMessage::SetValue(8));
+
+    let utx = UnsignedTransaction::new(
+        encoded_message.clone(),
+        config_value!("CHAIN_ID"),
+        max_priority_fee_bips,
+        200_000,
+        nonce,
+        None,
+    );
+
+    TransactionType::<ValueSetter<S>, S>::pre_signed(utx, signer.private_key())
+}
+
+#[derive(PartialEq, Eq)]
+enum TxStatus {
+    Valid,
+    BadNonce,
+    BadChainId,
+    // TODO add more conditions
+}
+
+impl TxStatus {
+    fn nb_of_valid_txs(statuses: &[TxStatus]) -> usize {
+        statuses
+            .iter()
+            .filter(|s| **s == TxStatus::Valid)
+            .collect::<Vec<_>>()
+            .len()
+    }
+
+    fn nb_of_skipped_txs(statuses: &[TxStatus]) -> usize {
+        statuses
+            .iter()
+            .filter(|s| **s != TxStatus::Valid)
+            .collect::<Vec<_>>()
+            .len()
+    }
+}
+
+fn create_txs(
+    statuses: &[TxStatus],
+    max_priority_fee_bips: PriorityFeeBips,
+    admin: &TestUser<S>,
+) -> Vec<TransactionType<ValueSetter<S>, S>> {
+    let mut nonce = 0;
+    let mut txs = Vec::new();
+    for status in statuses {
+        match status {
+            TxStatus::Valid => {
+                let tx = create_tx_valid(nonce, max_priority_fee_bips, admin);
+                txs.push(tx);
+                nonce += 1;
+            }
+            TxStatus::BadNonce => {
+                let tx = create_tx_valid(9999, max_priority_fee_bips, admin);
+                txs.push(tx);
+            }
+            TxStatus::BadChainId => {
+                let tx = create_tx_bad_chain_id(nonce, max_priority_fee_bips, admin);
+                txs.push(tx);
+            }
+        }
+    }
+    txs
 }
