60a53fc4288e2732190eb8c298cb116ce70cf6a3
Blazej Kolad <blazejkolad@gmail.com>
2024-11-06 13:31:03 +0100

    `rest-api-load-testing`: Users send requests concurrently. (#1823)

--- crates/utils/rest-api-load-testing/src/main.rs
+++ crates/utils/rest-api-load-testing/src/main.rs
@@ -3,6 +3,11 @@ use rest_api_load_testing::Requests;
 #[tokio::main]
 async fn main() {
     let requests = Requests::new("http://localhost:12346", vec!["ledger/slots/latest"]);
-    let report = rest_api_load_testing::start(requests).await;
-    println!("{report:?}");
+    let reports = rest_api_load_testing::start(requests).await;
+
+    for report in reports {
+        for m in report.measurements {
+            println!("{:?} {:?}", m.time, m.output);
+        }
+    }
 }
