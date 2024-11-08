c0f0bc6cb752a85754f9e37ede2f2a9a186fdb78
Filippo Neysofu Costa <filippo@sovlabs.io>
2024-11-08 17:30:08 +0100

    New associated type bound on `SequencerSpec` for type safety (#1837)
    * Additional type bound on `SequencerSpec` for type safety
    
    Signed-off-by: Filippo Costa <filippo@sovlabs.io>
    
    * Expand on comment
    
    Signed-off-by: Filippo Costa <filippo@sovlabs.io>
    
    ---------
    
    Signed-off-by: Filippo Costa <filippo@sovlabs.io>

--- crates/full-node/sov-sequencer/src/spec.rs
+++ crates/full-node/sov-sequencer/src/spec.rs
@@ -1,7 +1,7 @@
 use std::marker::PhantomData;
 
 use serde::de::DeserializeOwned;
-use sov_modules_api::{StoredEvent, TxReceiptContents};
+use sov_modules_api::{Spec, StoredEvent, TxReceiptContents};
 use sov_rollup_interface::node::da::DaService;
 
 use crate::batch_builders::BatchBuilder;
@@ -14,7 +14,10 @@ pub trait SequencerSpec: Clone + Send + Sync + 'static {
     type BatchBuilder: BatchBuilder;
     /// The [`DaService`] that the sequencer uses to communicate with the DA
     /// layer.
-    type Da: DaService;
+    ///
+    /// Its [`DaSpec`](sov_modules_api::DaSpec) **MUST** be the same one
+    /// specified by [`SequencerSpec::BatchBuilder`].
+    type Da: DaService<Spec = <<Self::BatchBuilder as BatchBuilder>::Spec as Spec>::Da>;
     /// The type of the batch receipt that the rollup stores in
     /// [`sov_db::ledger_db::LedgerDb`].
     type BatchReceipt: DeserializeOwned + Send + Sync;
@@ -37,7 +40,7 @@ impl<B, Da, BatchReceipt, TxReceipt, Event> SequencerSpec
     for GenericSequencerSpec<B, Da, BatchReceipt, TxReceipt, Event>
 where
     B: BatchBuilder,
-    Da: DaService,
+    Da: DaService<Spec = <<B as BatchBuilder>::Spec as Spec>::Da>,
     BatchReceipt: DeserializeOwned + Send + Sync + 'static,
     TxReceipt: TxReceiptContents,
     Event: TryFrom<(u64, StoredEvent), Error = anyhow::Error>
