162a0e9bfc7cf043deccdbb9bc4c34bfaab893d8
Blazej Kolad <blazejkolad@gmail.com>
2024-11-27 12:24:10 +0100

    Add query for the `token_id` in the rest benchmark (#1937)

--- examples/demo-rollup/rest-api-load-testing/src/main.rs
+++ examples/demo-rollup/rest-api-load-testing/src/main.rs
@@ -1,18 +1,26 @@
 use helpers::create_batch;
 use rest_api_load_testing::Requests;
+use sov_modules_api::Spec;
+use sov_test_utils::TestSpec;
 
 const PRIVATE_KEYS_FILE: &str = "../../test-data/keys/token_deployer_private_key.json";
 const URL: &str = "http://localhost:12346";
 
 #[tokio::main]
 async fn main() {
-    // To check available endpoints: http://localhost:12346/swagger-ui/
-    // TODO #1867
-    let _token_id = create_batch(PRIVATE_KEYS_FILE, URL.to_string()).await;
+    let seq_da_address = "0000000000000000000000000000000000000000000000000000000000000000";
+    let credentials_id = "0xfea6ac5b8751120fb62fff67b54d2eac66aef307c7dde1d394dea1e09e43dd44";
+
+    let (address, token_id) = create_batch(PRIVATE_KEYS_FILE, URL.to_string()).await;
 
     let mut all_endpoints = Vec::new();
     all_endpoints.append(&mut ledger_endpoints());
-    all_endpoints.append(&mut module_endpoints());
+    all_endpoints.append(&mut module_endpoints(
+        &address,
+        seq_da_address,
+        credentials_id,
+        &token_id,
+    ));
     all_endpoints.append(&mut rollup_endpoints());
 
     let requests = Requests::new(URL, all_endpoints);
@@ -20,7 +28,7 @@ async fn main() {
     summary.print_summary();
 }
 
-fn ledger_endpoints() -> Vec<&'static str> {
+fn ledger_endpoints() -> Vec<String> {
     // TODO: To test the commented endpoints, we need to send some transactions to the full node. #1807
     vec![
         "ledger/slots/latest",
@@ -28,56 +36,65 @@ fn ledger_endpoints() -> Vec<&'static str> {
         "ledger/batches/0/txs/0",
         "ledger/batches/0/txs/0/events/0",
         "ledger/events/0",
-        //"ledger/slots/{slotId}/batches/{batchOffset}",
+        //" ledger/slots/{slotId}/batches/{batchOffset}",
         // "ledger/slots/{slotId}/batches/{batchOffset}/txs/{txOffset}/events/{eventOffset}",
-        // "ledger/slots/{slotId}/events",
+        "ledger/slots/0/events",
         "ledger/txs/0",
         "ledger/txs/0/events/0",
     ]
+    .into_iter()
+    .map(|s| s.to_string())
+    .collect()
 }
 
-fn module_endpoints() -> Vec<&'static str> {
+fn module_endpoints(
+    address: &<TestSpec as Spec>::Address,
+    seq_da_address: &str,
+    credentials_id: &str,
+    token_id: &str,
+) -> Vec<String> {
     // TODO: To test the commented endpoints, we need to send some transactions to the full node. #1807
+
     vec![
         // Accounts
-        "modules/accounts/state/accounts",
-        // "modules/accounts/state/accounts/items/{key}",
-        "modules/accounts/state/credential-ids",
-        // "modules/accounts/state/credential-ids/items/{key}",
+        "modules/accounts/state/accounts".to_string(),
+        format!("modules/accounts/state/accounts/items/{credentials_id}"),
+        "modules/accounts/state/credential-ids".to_string(),
+        format!("modules/accounts/state/credential-ids/items/{address}"),
         // Bank
-        // "modules/bank/tokens/{token_id}/balances/{address}",
-        // "modules/bank/tokens/{token_id}/total-supply",
+        format!("modules/bank/tokens/?token_name={token_id}/balances/&sender={address}"),
+        format!("modules/bank/tokens/{token_id}/total-supply").to_string(),
         // Nonces
-        "modules/nonces/state/nonces",
-        // "modules/nonces/state/nonces/items/{key}",
+        "modules/nonces/state/nonces".to_string(),
+        format!("modules/nonces/state/nonces/items/{credentials_id}"),
         // Sequencer
-        "modules/sequencer-registry/state/allowed-sequencers",
-        //"modules/sequencer-registry/state/allowed-sequencers/items/{key}",
-        "modules/sequencer-registry/state/preferred-sequencer",
+        "modules/sequencer-registry/state/allowed-sequencers".to_string(),
+        format!("modules/sequencer-registry/state/allowed-sequencers/items/{seq_da_address}"),
+        "modules/sequencer-registry/state/preferred-sequencer".to_string(),
         // ChainState
-        "modules/chain-state/state/genesis-da-height",
-        "modules/chain-state/state/inner-code-commitment",
-        "modules/chain-state/state/next-visible-rollup-height",
-        "modules/chain-state/state/next-visible-rollup-height",
-        "modules/chain-state/state/operating-mode",
-        "modules/chain-state/state/outer-code-commitment",
-        "modules/chain-state/state/slots",
-        // "modules/chain-state/state/slots/items/{index}",
-        "modules/chain-state/state/state-roots",
-        // "modules/chain-state/state/state-roots/items/{index}",
-        "modules/chain-state/state/time",
-        "modules/chain-state/state/true-rollup-height",
-        "modules/chain-state/state/true-to-visible-rollup-height-history",
-        "modules/chain-state/state/true-to-visible-rollup-height-history",
+        "modules/chain-state/state/genesis-da-height".to_string(),
+        "modules/chain-state/state/inner-code-commitment".to_string(),
+        "modules/chain-state/state/next-visible-rollup-height".to_string(),
+        "modules/chain-state/state/next-visible-rollup-height".to_string(),
+        "modules/chain-state/state/operating-mode".to_string(),
+        "modules/chain-state/state/outer-code-commitment".to_string(),
+        "modules/chain-state/state/slots".to_string(),
+        "modules/chain-state/state/slots/items/0".to_string(),
+        "modules/chain-state/state/state-roots".to_string(),
+        "modules/chain-state/state/state-roots/items/0".to_string(),
+        "modules/chain-state/state/time".to_string(),
+        "modules/chain-state/state/true-rollup-height".to_string(),
+        "modules/chain-state/state/true-to-visible-rollup-height-history".to_string(),
     ]
 }
 
-fn rollup_endpoints() -> Vec<&'static str> {
+fn rollup_endpoints() -> Vec<String> {
     // TODO: To test the commented endpoints, we need to send some transactions to the full node. #1807
     vec![
-        "rollup/base-fee-per-gas/latest",
-        //"rollup/addresses/{address}/dedup",
-        "rollup/sync-status",
+        "rollup/base-fee-per-gas/latest".to_string(),
+        // TODO https://github.com/Sovereign-Labs/sovereign-sdk-wip/issues/1936
+        // format!("rollup/addresses/{address}/dedup"),
+        "rollup/sync-status".to_string(),
     ]
 }
 
@@ -86,7 +103,10 @@ mod helpers {
 
     use anyhow::Context;
     use demo_stf::runtime::{Runtime, RuntimeCall};
+    use sov_bank::types::TokenIdResponse;
     use sov_cli::wallet_state::PrivateKeyAndAddress;
+    use sov_modules_api::prelude::serde::de::DeserializeOwned;
+    use sov_modules_api::rest::utils::ResponseObject;
     use sov_modules_api::transaction::Transaction;
     use sov_modules_api::{Runtime as RuntimeTrait, SafeVec, Spec};
     use sov_test_utils::{default_test_signed_transaction, TestPrivateKey, TestSpec};
@@ -113,14 +133,14 @@ mod helpers {
                 .unwrap();
         }
 
-        async fn get(&self, url: String) -> String {
+        async fn get<T: DeserializeOwned>(&self, url: String) -> T {
             self.0
                 .client()
                 .get(url)
                 .send()
                 .await
                 .unwrap()
-                .text()
+                .json::<T>()
                 .await
                 .unwrap()
         }
@@ -143,7 +163,10 @@ mod helpers {
         default_test_signed_transaction(key, &msg, nonce, &Runtime::<TestSpec>::CHAIN_HASH)
     }
 
-    pub(crate) async fn create_batch(private_key_file: &str, url: String) -> String {
+    pub(crate) async fn create_batch(
+        private_key_file: &str,
+        url: String,
+    ) -> (<TestSpec as Spec>::Address, String) {
         let client = Client::new(&url);
         let keys = PrivateKeyAndAddress::<TestSpec>::from_json_file(Path::new(private_key_file))
             .context(format!("File does not exist: {:?}", private_key_file))
@@ -152,10 +175,13 @@ mod helpers {
         let sender = keys.address;
         let tx = build_create_token_tx(&priv_key, 0, 100);
         client.send_transactions(&[tx]).await;
-        client.get(query_token_id(url, sender)).await
+        let token_id_resp: ResponseObject<TokenIdResponse> =
+            client.get(query_token_id(&url, sender)).await;
+
+        (sender, token_id_resp.data.unwrap().token_id.to_string())
     }
 
-    fn query_token_id(url: String, sender: <TestSpec as Spec>::Address) -> String {
+    fn query_token_id(url: &str, sender: <TestSpec as Spec>::Address) -> String {
         format!(
             "{}/modules/bank/tokens?token_name={}&sender={}",
             url, TOKEN_NAME, sender
