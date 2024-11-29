61da154d51bcd94d03cfbf86ba57000bb8449aeb
Nikolai Golub <nikolai@sovlabs.io>
2024-11-29 14:57:59 +0100

    Convert sov-metrics crate to be native only (#1948)
    * Convert sov-metrics crate to be native only
    
    * Add CHANGELOG.md entry
    
    * Add accidentally removed tokio features

--- crates/full-node/sov-metrics/src/lib.rs
+++ crates/full-node/sov-metrics/src/lib.rs
@@ -4,10 +4,8 @@
 mod influxdb;
 mod prometheus;
 
-#[cfg(feature = "native")]
-pub use influxdb::{init_metrics_tracker, MonitoringConfig};
 pub use influxdb::{
-    track_metrics, MetricsTracker, RunnerMetrics, SlotProcessingMetrics, TransactionEffect,
-    TransactionProcessingMetrics,
+    init_metrics_tracker, track_metrics, MetricsTracker, MonitoringConfig, RunnerMetrics,
+    SlotProcessingMetrics, TransactionEffect, TransactionProcessingMetrics,
 };
 pub use prometheus::{update_metrics, Metrics};
