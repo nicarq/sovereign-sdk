aa6f8390d11221df0b1dfc1ea0e54631f86f6b1a
Blazej Kolad <blazejkolad@gmail.com>
2024-12-04 20:32:20 +0100

    `Outer Circuit`: Adds verification for the inner circuit. (#1971)

--- crates/full-node/sov-aggregated-proof/src/main.rs
+++ crates/full-node/sov-aggregated-proof/src/main.rs
@@ -1,22 +1,13 @@
-use hex::FromHex;
-use risc0_zkp::core::digest::Digest;
 use sov_aggregated_proof::check_receipts;
 
 fn main() {
-    let method_id =
-        Digest::from_hex("665839999d6b39fff2bfce839e709d4eb0eb75cdcda76219729fb81b5fd381ca")
-            .unwrap();
-
-    check_receipts(
-        vec![
-            "data/inner_0.proof",
-            "data/inner_1.proof",
-            "data/inner_2.proof",
-            "data/inner_3.proof",
-            "data/inner_4.proof",
-            "data/inner_5.proof",
-        ],
-        method_id,
-    )
+    check_receipts(vec![
+        "data/inner_0.proof",
+        "data/inner_1.proof",
+        "data/inner_2.proof",
+        "data/inner_3.proof",
+        "data/inner_4.proof",
+        "data/inner_5.proof",
+    ])
     .unwrap();
 }
