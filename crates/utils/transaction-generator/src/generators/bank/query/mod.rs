10023bc6702c256f087de6799aa69930a8c4ea83
theochap <80177219+theochap@users.noreply.github.com>
2024-12-09 11:21:50 +0100

    Small refactor to harness before adding tests (#2016)
    * Refactor + remove unused traits
    
    * Small refactor change
    
    * Fix test
    
    * Merging with nightly
    
    * Lints

--- crates/utils/transaction-generator/src/generators/bank/query/mod.rs
+++ crates/utils/transaction-generator/src/generators/bank/query/mod.rs
@@ -1,13 +1,1 @@
 pub mod http;
-use sov_bank::TokenId;
-use sov_modules_api::prelude::axum::async_trait;
-use sov_modules_api::Spec;
-
-/// A trait which allows access to state values needed by the bank generator
-#[async_trait]
-pub trait BankClient<S: Spec> {
-    /// Gets the user's balance of the given TokenId
-    async fn get_balance(&self, user: &S::Address, token: TokenId) -> sov_bank::Amount;
-    /// Gets the total supply of the given TokenId
-    async fn get_total_supply(&self, token: &TokenId) -> sov_bank::Amount;
-}
