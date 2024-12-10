6220d0e95344018a6699943619a1875f46e575ae
Cem Özer <cem@sovlabs.io>
2024-12-10 13:32:31 +0100

    Add standard rollup tx to evm test runtime (#2029)
    * Update EVM module test runtime to support standard transactions
    
    * Run linter
    
    * Remove commented files

--- crates/module-system/module-implementations/sov-evm/tests/integration/runtime.rs
+++ crates/module-system/module-implementations/sov-evm/tests/integration/runtime.rs
@@ -1,6 +1,7 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use sov_evm::Evm;
 use sov_modules_api::capabilities::{AuthorizationData, TransactionAuthenticator};
+use sov_modules_api::runtime::Runtime;
 use sov_modules_api::{DispatchCall, ProvableStateReader, RawTx, Spec};
 use sov_state::User;
 use sov_test_utils::{generate_bare_runtime, TestSpec};
@@ -15,47 +16,11 @@ generate_bare_runtime! {
     kernel_type: sov_kernels::basic::BasicKernel<'a, S>
 }
 
-/// A converter from an Ethereum address to a rollup address.
-pub struct EthereumToRollupAddressConverter([u8; 20]);
-
-impl From<sov_evm::RethAddress> for EthereumToRollupAddressConverter {
-    fn from(address: sov_evm::RethAddress) -> Self {
-        Self(address.into())
-    }
-}
-
-impl From<sov_evm::spec::EthereumAddress> for EthereumToRollupAddressConverter {
-    fn from(address: sov_evm::spec::EthereumAddress) -> Self {
-        Self(address.into())
-    }
-}
-
-impl TryInto<reth_primitives::Address> for EthereumToRollupAddressConverter {
-    type Error = anyhow::Error;
-
-    fn try_into(self) -> Result<reth_primitives::Address, Self::Error> {
-        Ok(reth_primitives::Address::from(self.0))
-    }
-}
-
-impl TryInto<sov_evm::spec::EthereumAddress> for EthereumToRollupAddressConverter {
-    type Error = anyhow::Error;
-
-    fn try_into(self) -> Result<sov_evm::spec::EthereumAddress, Self::Error> {
-        Ok(reth_primitives::Address::from(self.0).into())
-    }
-}
-
-impl<H> TryInto<sov_modules_api::Address<H>> for EthereumToRollupAddressConverter {
-    type Error = anyhow::Error;
-
-    fn try_into(self) -> Result<sov_modules_api::Address<H>, Self::Error> {
-        anyhow::bail!("Not implemented")
-    }
-}
-
 #[derive(std::fmt::Debug, Clone, BorshDeserialize, BorshSerialize)]
-pub struct AuthenticatorInput(sov_modules_api::RawTx);
+pub enum Auth {
+    Evm(sov_modules_api::RawTx),
+    Standard(sov_modules_api::RawTx),
+}
 
 impl<S: Spec> TransactionAuthenticator<S> for TestRuntime<S>
 where
@@ -65,11 +30,11 @@ where
 
     type AuthorizationData = AuthorizationData<S>;
 
-    type Input = AuthenticatorInput;
+    type Input = Auth;
 
     fn authenticate<Accessor: ProvableStateReader<User, Spec = S>>(
         &self,
-        tx: &AuthenticatorInput,
+        tx: &Self::Input,
         state: &mut Accessor,
     ) -> Result<
         sov_modules_api::capabilities::AuthenticationOutput<
@@ -79,16 +44,33 @@ where
         >,
         sov_modules_api::capabilities::AuthenticationError,
     > {
-        let (tx_and_raw_hash, auth_data, runtime_call) =
-            sov_evm::authenticate::<_, _, EthereumToRollupAddressConverter>(&tx.0.data, state)?;
-        let call = TestRuntimeCall::Evm(runtime_call);
+        match tx {
+            Auth::Evm(tx) => {
+                let (tx_and_raw_hash, auth_data, runtime_call) =
+                    sov_evm::authenticate::<_, _, EthereumToRollupAddressConverter>(
+                        &tx.data, state,
+                    )?;
+                let call = TestRuntimeCall::Evm(runtime_call);
 
-        Ok((tx_and_raw_hash, auth_data, call))
+                Ok((tx_and_raw_hash, auth_data, call))
+            }
+            Auth::Standard(tx) => {
+                let (tx_and_raw_hash, auth_data, runtime_call) =
+                    sov_modules_api::capabilities::authenticate::<_, S, Self>(
+                        &tx.data,
+                        &Self::CHAIN_HASH,
+                        state,
+                    )
+                    .unwrap();
+
+                Ok((tx_and_raw_hash, auth_data, runtime_call))
+            }
+        }
     }
 
     fn authenticate_unregistered<Accessor: ProvableStateReader<User, Spec = S>>(
         &self,
-        _tx: &AuthenticatorInput,
+        _tx: &Self::Input,
         _state: &mut Accessor,
     ) -> Result<
         sov_modules_api::capabilities::AuthenticationOutput<
@@ -102,7 +84,36 @@ where
     }
 
     fn add_standard_auth(tx: RawTx) -> Self::Input {
-        AuthenticatorInput(tx)
+        Auth::Standard(tx)
+    }
+}
+
+impl<S: Spec> sov_evm::EthereumAuthenticator<S> for TestRuntime<S>
+where
+    EthereumToRollupAddressConverter: TryInto<S::Address>,
+{
+    fn add_ethereum_auth(tx: RawTx) -> <Self as TransactionAuthenticator<S>>::Input {
+        Auth::Evm(tx)
+    }
+}
+
+/// A converter from an Ethereum address to a rollup address.
+pub struct EthereumToRollupAddressConverter(
+    /// The raw bytes of the ethereum address.
+    pub [u8; 20],
+);
+
+impl From<sov_evm::RethAddress> for EthereumToRollupAddressConverter {
+    fn from(address: sov_evm::RethAddress) -> Self {
+        Self(address.into())
+    }
+}
+
+impl<H> TryInto<sov_modules_api::Address<H>> for EthereumToRollupAddressConverter {
+    type Error = anyhow::Error;
+
+    fn try_into(self) -> Result<sov_modules_api::Address<H>, Self::Error> {
+        anyhow::bail!("Not implemented")
     }
 }
 
