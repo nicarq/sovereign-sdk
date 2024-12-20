0e5332379e67728f2c1725c75f535075e8721244
Nikolai Golub <nikolai@sovlabs.io>
2024-12-20 22:17:00 +0100

    ZK VM memory tracking (#2074)
    * Attempt to use embedded-malloc
    
    * Use hacky approach
    
    * Cleaning up
    
    * Cleaning up
    
    * Revert "Cycle counts for blob selection and validation (#2072)"
    
    This reverts commit 43330ff6b4a6f53bb2bfc53957355f7adc60373c.
    
    * Fix dashboard
    
    * Fixes
    
    * Remove risc0 deps that spilled all over
    
    * Revert Makefile change
    
    * Update to commit hash from main
    
    * update lock files

--- crates/adapters/risc0/src/metrics.rs
+++ crates/adapters/risc0/src/metrics.rs
@@ -1,22 +1,7 @@
 //! Defines utilities for collecting runtime metrics from inside a Risc0 VM
-use anyhow::Context;
 use risc0_zkvm::Bytes;
 /// The name of the syscall we use to collect metrics from the Risc0 VM.
 pub use sov_cycle_utils::risc0::SYSCALL_NAME_METRICS;
-
-/// Deserialize a `Bytes` into a null-separated `(String, u64)` tuple. This function
-/// expects its arguments to match the format of arguments to Risc0's io callbacks.
-fn deserialize_custom(serialized: Bytes) -> anyhow::Result<(String, u64)> {
-    let null_pos = serialized
-        .iter()
-        .position(|&b| b == 0)
-        .context("Could not find separator in provided bytes")?;
-    let (string_bytes, size_bytes_with_null) = serialized.split_at(null_pos);
-    let size_bytes = &size_bytes_with_null[1..]; // Skip the null terminator
-    let string = String::from_utf8(string_bytes.to_vec())?;
-    let size = u64::from_le_bytes(size_bytes.try_into()?); // Convert bytes back into usize
-    Ok((string, size))
-}
 
 /// A custom callback for extracting metrics from the Risc0 zkvm.
 ///
@@ -24,12 +9,14 @@ fn deserialize_custom(serialized: Bytes) -> anyhow::Result<(String, u64)> {
 /// in the Risc0 VM and invoked whenever a function annotated with the [`risc0-cycle-utils::cycle_tracker`]
 /// macro is invoked.
 pub fn metrics_callback(input: Bytes) -> anyhow::Result<Bytes> {
-    let (metric, cycles_count) = deserialize_custom(input)?;
+    let (metric, cycles_count, free_heap_bytes) =
+        sov_cycle_utils::risc0::deserialize_metrics_call(input.as_ref())?;
     sov_cycle_utils::increment_metric(metric.clone(), cycles_count);
     sov_metrics::track_metrics(|tracker| {
-        tracker.track_zkvm_metric(sov_metrics::ZkVmCycleCount {
+        tracker.track_zkvm_metric(sov_metrics::ZkVmExecutionChunk {
             name: metric,
             cycles_count,
+            free_heap_bytes,
         });
     });
     Ok(Bytes::new())
