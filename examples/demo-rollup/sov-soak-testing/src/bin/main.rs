a2743d815a575f04b1468be128cfedcebf8fac96
Ross Weir <29697678+ross-weir@users.noreply.github.com>
2025-01-28 22:34:34 +1100

    Configurable port for test rollup (#2301)
    * configurable port for soak test cli
    
    * default to 0 for axum port

--- examples/demo-rollup/sov-soak-testing/src/bin/main.rs
+++ examples/demo-rollup/sov-soak-testing/src/bin/main.rs
@@ -5,6 +5,11 @@ use tokio::signal::unix::SignalKind;
 
 #[derive(Parser)]
 struct Args {
+    #[arg(short, long, default_value_t = 12346)]
+    /// The port that the axum API server will listen on.
+    /// Defaults to 12346.
+    axum_port: u16,
+
     #[arg(short, long, default_value = "soak_data/")]
     storage_path: String,
 }
@@ -13,7 +18,7 @@ struct Args {
 async fn main() -> Result<(), anyhow::Error> {
     let _guard = sov_modules_rollup_blueprint::logging::initialize_logging();
     let args = Args::parse();
-    let (rollup, _) = setup_rollup(args.storage_path.into()).await;
+    let (rollup, _) = setup_rollup(args.storage_path.into(), args.axum_port).await;
 
     let mut terminate = tokio::signal::unix::signal(SignalKind::terminate())
         .expect("Failed to set up SIGTERM handler");
