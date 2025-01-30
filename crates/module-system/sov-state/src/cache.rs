a2f502ec16e1f11ccac829ce9a75ec2f2668bc14
Blazej Kolad <blazejkolad@gmail.com>
2025-01-30 11:33:49 +0100

    Remove unused code (#2322)

--- crates/module-system/sov-state/src/cache.rs
+++ crates/module-system/sov-state/src/cache.rs
@@ -17,27 +17,6 @@ pub enum IsValueCached {
     No,
 }
 
-/// An error when merging two cache values.
-#[derive(Debug, PartialEq, Eq, thiserror::Error)]
-pub enum MergeError {
-    /// Consecutive reads error.
-    #[error("consecutive reads are inconsistent: left read: {left:?}, right read: {right:?}")]
-    ReadThenRead {
-        /// Left-read associated cache value.
-        left: Option<SlotValue>,
-        /// Right-read associated cache value.
-        right: Option<SlotValue>,
-    },
-    /// A read operation is inconsistent with the previous write operation.
-    #[error("the read: {read:?} is in inconsistent with the previous write: {write:?}")]
-    WriteThenRead {
-        /// The associated write operation.
-        write: Option<SlotValue>,
-        /// The associated read operation.
-        read: Option<SlotValue>,
-    },
-}
-
 /// An error when reading from the cache.
 #[derive(Debug, Eq, PartialEq, thiserror::Error)]
 pub enum ReadError {
@@ -91,13 +70,6 @@ impl Access {
         }
     }
 
-    fn original_mut(&mut self) -> Option<&mut Option<SlotValue>> {
-        match self {
-            Access::Write { .. } => None,
-            Access::Read { original } | Access::ReadThenWrite { original, .. } => Some(original),
-        }
-    }
-
     fn modified_mut(&mut self) -> Option<&mut Option<SlotValue>> {
         match self {
             Access::Read { .. } => None,
@@ -130,31 +102,6 @@ impl Access {
                 *modified = write;
             }
         }
-    }
-
-    fn merge(&mut self, mut rhs: Self) -> Result<(), MergeError> {
-        if let Some(r_read) = rhs.original_mut() {
-            let l_last = self.last_value();
-            if l_last != r_read.as_ref() {
-                if self.modified().is_some() {
-                    return Err(MergeError::WriteThenRead {
-                        write: l_last.cloned(),
-                        read: r_read.take(),
-                    });
-                } else {
-                    return Err(MergeError::ReadThenRead {
-                        left: l_last.cloned(),
-                        right: r_read.take(),
-                    });
-                }
-            }
-        }
-
-        if let Some(r_write) = rhs.modified_mut() {
-            self.add_write(r_write.take());
-        }
-
-        Ok(())
     }
 }
 
@@ -236,61 +183,6 @@ impl CacheLog {
             }
         }
     }
-
-    /// Merges two cache logs in a way that preserves the first read (from self) and the last write (from rhs)
-    /// for the same key in both caches.
-    /// The merge succeeds if the first read in the right cache for a key 'k' is consistent with the last read/write
-    /// in the self cache.
-    ///
-    /// Example:
-    ///
-    /// Cache1:        Cache2:
-    ///     k1 => v1       k1 => v1'
-    ///     k2 => v2       k3 => v3
-    ///
-    /// Merged Cache:
-    ///     k1 => v1.merge(v1') <- preserves the first read and the last write for 'k1'
-    ///     k2 => v2
-    ///     k3 => v3
-    pub fn merge_left(&mut self, rhs: Self) -> Result<(), MergeError> {
-        self.merge_left_with_filter_map(rhs, Some)
-    }
-
-    /// Merges two cache logs in a way that preserves the first read (from self) and the last write (from rhs).
-    pub fn merge_writes_left(&mut self, rhs: Self) -> Result<(), MergeError> {
-        self.merge_left_with_filter_map(rhs, |(key, access)| match access {
-            Access::Read { .. } => None,
-            Access::ReadThenWrite { modified, .. } | Access::Write { modified } => {
-                Some((key, Access::Write { modified }))
-            }
-        })
-    }
-
-    /// Merges two cache logs in a way that preserves the first read (from self) and the last write (from rhs).
-    pub fn merge_reads_left(&mut self, rhs: Self) -> Result<(), MergeError> {
-        self.merge_left_with_filter_map(rhs, |(key, access)| match access {
-            Access::Write { .. } => None,
-            Access::Read { original } | Access::ReadThenWrite { original, .. } => {
-                Some((key, Access::Read { original }))
-            }
-        })
-    }
-
-    fn merge_left_with_filter_map<F: FnMut((SlotKey, Access)) -> Option<(SlotKey, Access)>>(
-        &mut self,
-        rhs: Self,
-        filter: F,
-    ) -> Result<(), MergeError> {
-        for (rhs_key, rhs_access) in rhs.log.into_iter().filter_map(filter) {
-            match self.log.get_mut(&rhs_key) {
-                Some(self_access) => self_access.merge(rhs_access)?,
-                None => {
-                    self.log.insert(rhs_key, rhs_access);
-                }
-            };
-        }
-        Ok(())
-    }
 }
 
 /// Caches reads and writes for a (key, value) pair. On the first read the value is fetched
@@ -350,21 +242,6 @@ impl<N: ProvableCompileTimeNamespace> ProvableStorageCache<N> {
     /// Deletes a keyed value from the cache.
     pub fn delete(&mut self, key: &SlotKey) -> IsValueCached {
         self.tx_cache.add_write(key.clone(), None)
-    }
-
-    /// Merges the provided `ProvableStorageCache` into this one.
-    pub fn merge_left(&mut self, rhs: Self) -> Result<(), MergeError> {
-        self.tx_cache.merge_left(rhs.tx_cache)
-    }
-
-    /// Merges the reads of the provided `ProvableStorageCache` into this one.
-    pub fn merge_reads_left(&mut self, rhs: Self) -> Result<(), MergeError> {
-        self.tx_cache.merge_reads_left(rhs.tx_cache)
-    }
-
-    /// Merges the writes of the provided `ProvableStorageCache` into this one.
-    pub fn merge_writes_left(&mut self, rhs: Self) -> Result<(), MergeError> {
-        self.tx_cache.merge_writes_left(rhs.tx_cache)
     }
 
     fn add_read(&mut self, key: SlotKey, value: Option<SlotValue>) {
@@ -483,58 +360,6 @@ mod tests {
         CacheEntry::new(create_key(key), create_value(value))
     }
 
-    #[derive(Clone)]
-    enum ReadWrite {
-        Read(CacheEntry),
-        Write(CacheEntry),
-    }
-
-    impl ReadWrite {
-        fn get_value(self) -> CacheEntry {
-            match self {
-                ReadWrite::Read(r) => r,
-                ReadWrite::Write(w) => w,
-            }
-        }
-
-        fn check_cache_consistency(self, rhs: Self, merged: &CacheLog) {
-            match (self, rhs) {
-                (ReadWrite::Read(left_read), ReadWrite::Read(right_read)) => {
-                    assert_eq!(left_read, right_read);
-                    let value = merged.get_value(&left_read.key).get();
-                    assert_eq!(left_read.value, value);
-                }
-                (ReadWrite::Write(left_write), ReadWrite::Read(right_write)) => {
-                    assert_eq!(left_write, right_write);
-                    let value = merged.get_value(&left_write.key).get();
-                    assert_eq!(left_write.value, value);
-                }
-                (_, ReadWrite::Write(right_write)) => {
-                    let value = merged.get_value(&right_write.key).get();
-                    assert_eq!(right_write.value, value);
-                }
-            }
-        }
-    }
-
-    impl CacheLog {
-        fn add_to_cache(&mut self, rw: ReadWrite) -> Result<(), ReadError> {
-            match rw {
-                ReadWrite::Read(r) => self.add_read(r.key, r.value),
-                ReadWrite::Write(w) => {
-                    self.add_write(w.key, w.value);
-                    Ok(())
-                }
-            }
-        }
-    }
-
-    #[derive(Clone)]
-    struct TestCase {
-        left: Option<ReadWrite>,
-        right: Option<ReadWrite>,
-    }
-
     #[test]
     fn test_add_read() {
         let mut cache = CacheLog::default();
@@ -560,145 +385,6 @@ mod tests {
         );
     }
 
-    #[test]
-    fn test_merge_ok() {
-        let test_cases = vec![
-            TestCase {
-                left: Some(ReadWrite::Read(new_cache_entry(1, 11))),
-                right: Some(ReadWrite::Read(new_cache_entry(1, 11))),
-            },
-            TestCase {
-                left: Some(ReadWrite::Read(new_cache_entry(2, 12))),
-                right: Some(ReadWrite::Write(new_cache_entry(2, 22))),
-            },
-            TestCase {
-                left: Some(ReadWrite::Write(new_cache_entry(3, 13))),
-                right: Some(ReadWrite::Write(new_cache_entry(3, 23))),
-            },
-            TestCase {
-                left: Some(ReadWrite::Write(new_cache_entry(4, 14))),
-                right: None,
-            },
-            TestCase {
-                left: None,
-                right: Some(ReadWrite::Read(new_cache_entry(5, 25))),
-            },
-            TestCase {
-                left: None,
-                right: Some(ReadWrite::Write(new_cache_entry(6, 25))),
-            },
-            TestCase {
-                left: Some(ReadWrite::Write(new_cache_entry(7, 17))),
-                right: Some(ReadWrite::Read(new_cache_entry(7, 17))),
-            },
-        ];
-
-        test_merge_ok_helper(test_cases);
-    }
-
-    #[test]
-    fn test_merge_fail() {
-        let test_cases = vec![
-            TestCase {
-                left: Some(ReadWrite::Read(new_cache_entry(1, 11))),
-                // The read is inconsistent with the previous read.
-                right: Some(ReadWrite::Read(new_cache_entry(1, 12))),
-            },
-            TestCase {
-                left: Some(ReadWrite::Write(new_cache_entry(2, 12))),
-                // The read is inconsistent with the previous write.
-                right: Some(ReadWrite::Read(new_cache_entry(2, 22))),
-            },
-        ];
-
-        let result = test_merge_helper(test_cases);
-        assert!(result.is_err());
-    }
-
-    #[test_strategy::proptest]
-    fn test_merge_fuzz(s: u8) {
-        let num_cases = 15;
-        let mut testvec = Vec::with_capacity(num_cases);
-
-        for i in 0..num_cases {
-            testvec.push(s.wrapping_add(i as u8));
-        }
-
-        let test_cases = vec![
-            TestCase {
-                left: Some(ReadWrite::Read(new_cache_entry(testvec[0], testvec[1]))),
-                right: Some(ReadWrite::Read(new_cache_entry(testvec[0], testvec[1]))),
-            },
-            TestCase {
-                left: Some(ReadWrite::Read(new_cache_entry(testvec[2], testvec[3]))),
-                right: Some(ReadWrite::Write(new_cache_entry(testvec[2], testvec[4]))),
-            },
-            TestCase {
-                left: Some(ReadWrite::Write(new_cache_entry(testvec[5], testvec[6]))),
-                right: Some(ReadWrite::Write(new_cache_entry(testvec[5], testvec[7]))),
-            },
-            TestCase {
-                left: Some(ReadWrite::Write(new_cache_entry(testvec[8], testvec[9]))),
-                right: None,
-            },
-            TestCase {
-                left: None,
-                right: Some(ReadWrite::Read(new_cache_entry(testvec[10], testvec[11]))),
-            },
-            TestCase {
-                left: None,
-                right: Some(ReadWrite::Write(new_cache_entry(testvec[12], testvec[11]))),
-            },
-            TestCase {
-                left: Some(ReadWrite::Write(new_cache_entry(testvec[13], testvec[14]))),
-                right: Some(ReadWrite::Read(new_cache_entry(testvec[13], testvec[14]))),
-            },
-        ];
-
-        test_merge_ok_helper(test_cases);
-    }
-
-    fn test_merge_ok_helper(test_cases: Vec<TestCase>) {
-        let result = test_merge_helper(test_cases.clone());
-        assert!(result.is_ok());
-
-        let merged = result.unwrap();
-        assert_eq!(merged.log.len(), test_cases.len());
-
-        for TestCase { left, right } in test_cases {
-            match (left, right) {
-                (None, None) => unreachable!(),
-                (Some(rw), None) | (None, Some(rw)) => {
-                    let entry = rw.get_value();
-                    let value = merged.get_value(&entry.key).get();
-                    assert_eq!(entry.value, value);
-                }
-                (Some(left_rw), Some(right_rw)) => {
-                    left_rw.check_cache_consistency(right_rw, &merged);
-                }
-            }
-        }
-    }
-
-    fn test_merge_helper(test_cases: Vec<TestCase>) -> Result<CacheLog, MergeError> {
-        let mut left_cache = CacheLog::default();
-        let mut right_cache = CacheLog::default();
-
-        for TestCase { left, right } in test_cases {
-            match (left, right) {
-                (None, None) => {}
-                (None, Some(rw)) => right_cache.add_to_cache(rw).unwrap(),
-                (Some(rw), None) => left_cache.add_to_cache(rw).unwrap(),
-                (Some(left_rw), Some(right_rw)) => {
-                    left_cache.add_to_cache(left_rw).unwrap();
-                    right_cache.add_to_cache(right_rw).unwrap();
-                }
-            }
-        }
-
-        left_cache.merge_left(right_cache)?;
-        Ok(left_cache)
-    }
     #[test]
     fn test_access_read_write() {
         let original_value = create_value(1);
@@ -748,142 +434,5 @@ mod tests {
             assert_eq!(access.last_value(), new_value.as_ref());
             assert_eq!(access, Access::write(new_value));
         }
-    }
-
-    #[test]
-    fn test_access_merge() {
-        let first_read = 1;
-        let mut value = create_value(first_read);
-        let mut left = Access::read(value.clone());
-
-        let last_write = 10;
-        for i in 2..last_write + 1 {
-            left.merge(Access::read(value.clone())).unwrap();
-
-            value = create_value(i);
-            left.merge(Access::write(value.clone())).unwrap();
-        }
-
-        assert_eq!(
-            left,
-            Access::ReadThenWrite {
-                original: create_value(first_read),
-                modified: create_value(last_write)
-            }
-        );
-    }
-
-    #[test]
-    fn test_err_merge_left_read_neq_right_read() {
-        let first_read = 1;
-        let value = create_value(first_read);
-        let left = &mut Access::read(value.clone());
-
-        let second_read = 2;
-        let value2 = create_value(second_read);
-
-        assert_eq!(
-            left.merge(Access::read(value2.clone())),
-            Err(MergeError::ReadThenRead {
-                left: value,
-                right: value2,
-            })
-        );
-    }
-
-    #[test]
-    fn test_err_merge_left_read_neq_right_orig() {
-        let first_read = 1;
-        let value = create_value(first_read);
-        let left = &mut Access::read(value.clone());
-
-        let second_read = 2;
-        let value2 = create_value(second_read);
-        let right = Access::ReadThenWrite {
-            original: value2.clone(),
-            modified: value.clone(),
-        };
-
-        assert_eq!(
-            left.merge(right),
-            Err(MergeError::ReadThenRead {
-                left: value,
-                right: value2,
-            })
-        );
-    }
-
-    #[test]
-    fn test_err_merge_left_mod_neq_right_read() {
-        let first_read = 1;
-        let value = create_value(first_read);
-
-        let second_read = 2;
-        let value2 = create_value(second_read);
-
-        let left = &mut Access::ReadThenWrite {
-            original: value2.clone(),
-            modified: value.clone(),
-        };
-
-        let right = Access::read(value2.clone());
-
-        assert_eq!(
-            left.merge(right),
-            Err(MergeError::WriteThenRead {
-                write: value,
-                read: value2,
-            })
-        );
-    }
-
-    #[test]
-    fn test_err_merge_left_mod_neq_right_orig() {
-        let first_read = 1;
-        let value = create_value(first_read);
-
-        let second_read = 2;
-        let value2 = create_value(second_read);
-
-        let left = &mut Access::ReadThenWrite {
-            original: value.clone(),
-            modified: value2.clone(),
-        };
-
-        let right = Access::ReadThenWrite {
-            original: value.clone(),
-            modified: value2.clone(),
-        };
-
-        assert_eq!(
-            left.merge(right),
-            Err(MergeError::WriteThenRead {
-                write: value2,
-                read: value,
-            })
-        );
-    }
-
-    #[test]
-    fn test_err_merge_left_right_neq_right_orig() {
-        let first_read = 1;
-        let value = create_value(first_read);
-
-        let second_read = 2;
-        let value2 = create_value(second_read);
-
-        let left = &mut Access::write(value.clone());
-        let right = Access::ReadThenWrite {
-            original: value2.clone(),
-            modified: value.clone(),
-        };
-
-        assert_eq!(
-            left.merge(right),
-            Err(MergeError::WriteThenRead {
-                write: value,
-                read: value2,
-            })
-        );
     }
 }
