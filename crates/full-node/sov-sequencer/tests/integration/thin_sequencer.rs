9f0523d3bbd02802a9ba66ac1406e10be913d632
Nikolai Golub <nikolai@sovlabs.io>
2025-02-11 13:46:15 +0100

    Remove transactions payload from `submit_batch` endpoint in stateless sequencer test. (#2374)
    * [no ci] Initial refactoring
    
    * Make it use only signle transaction so we don't deal with nonce issue
    
    * Revert debug changes
    
    * Aaaah!
    
    * Re-enabled automatic batch production
    
    * Do not use create_batch for std sequencer

--- crates/full-node/sov-sequencer/tests/integration/thin_sequencer.rs
+++ crates/full-node/sov-sequencer/tests/integration/thin_sequencer.rs
@@ -1,11 +1,13 @@
 use std::sync::Arc;
 
+use anyhow::Context;
 use base64::prelude::BASE64_STANDARD;
 use base64::Engine;
 use futures::StreamExt;
 use sov_api_spec::types::AcceptTxBody;
+use sov_blob_storage::config_deferred_slots_count;
 use sov_mock_da::storable::service::StorableMockDaService;
-use sov_mock_da::{BlockProducingConfig, MockAddress};
+use sov_mock_da::{BlockProducingConfig, MockAddress, MockBlock};
 use sov_modules_api::{RawTx, Runtime};
 use sov_modules_stf_blueprint::GenesisParams;
 use sov_rollup_interface::common::SafeVec;
@@ -22,12 +24,6 @@ use sov_test_utils::{
 generate_optimistic_runtime!(TestRuntime <=);
 
 type TestBlueprint = RtAgnosticBlueprint<TestSpec, TestRuntime<TestSpec>>;
-
-#[derive(Debug, Clone)]
-struct IterationCase {
-    via_accept_tx: usize,
-    via_submit: usize,
-}
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_thin_direct_same_transactions() -> anyhow::Result<()> {
@@ -50,6 +46,7 @@ async fn test_thin_direct_same_transactions() -> anyhow::Result<()> {
         .runtime
         .sequencer_registry
         .is_preferred_sequencer = false;
+
     let test_rollup = RollupBuilder::<TestBlueprint>::new(
         GenesisSource::CustomParams(genesis_params),
         BlockProducingConfig::Manual,
@@ -76,113 +73,68 @@ async fn test_thin_direct_same_transactions() -> anyhow::Result<()> {
     let mut slots = test_rollup.api_client.subscribe_slots().await?;
 
     let user = genesis_config.additional_accounts.first().unwrap();
-    let mut all_txs = generate_txs(user, 8);
+    // TODO: https://github.com/Sovereign-Labs/sovereign-sdk-wip/issues/947
+    //    Ideally we want to test multiple transactions, but this bug prevents it
+    // let all_txs = generate_txs(user, 10);
+    // for tx in &all_txs {
+    //     let tx_hash_accepted_a = accept_tx_in_rollup(&test_rollup.api_client, &tx).await?;
+    //     let tx_hash_accepted_b = accept_tx_in_rollup(test_sequencer_client, &tx).await?;
+    //     assert_eq!(tx_hash_accepted_a, tx_hash_accepted_b);
+    // }
 
-    let cases = vec![
-        // 1. submit 3 txs in `publishBatch` only
-        IterationCase {
-            via_accept_tx: 0,
-            via_submit: 3,
-        },
-        // 2. accept 3 txs, then empty `publishBatch`
-        IterationCase {
-            via_accept_tx: 3,
-            via_submit: 0,
-        },
-        // 3. accept 1 txs, then submit 1 tx in `publishBatch`
-        IterationCase {
-            via_accept_tx: 1,
-            via_submit: 1,
-        },
-    ];
+    let tx = generate_tx_with_nonce(user, 1);
+    let tx_hash_accepted_a = accept_tx_in_rollup(&test_rollup.api_client, &tx).await?;
+    let tx_hash_accepted_b = accept_tx_in_rollup(test_sequencer_client, &tx).await?;
+    assert_eq!(tx_hash_accepted_a, tx_hash_accepted_b);
 
-    for (idx, case) in cases.into_iter().enumerate() {
-        let IterationCase {
-            via_accept_tx,
-            via_submit,
-        } = case.clone();
-        let expected_total_hashes = via_accept_tx + via_submit;
-
-        let via_accept_txs = all_txs.drain(..via_accept_tx).collect::<Vec<_>>();
-        // Accept first
-        for tx in via_accept_txs {
-            let tx_hash_accepted_a = accept_tx_in_rollup(&test_rollup.api_client, &tx).await?;
-            let tx_hash_accepted_b = accept_tx_in_rollup(test_sequencer_client, &tx).await?;
-            assert_eq!(tx_hash_accepted_a, tx_hash_accepted_b);
-        }
-
-        // Submit batch
-        let via_submit_txs = all_txs.drain(..via_submit).collect::<Vec<_>>();
-        let tx_hashes_a = publish_batch_in_rollup(&test_rollup.api_client, &via_submit_txs).await?;
-        let tx_hashes_b = publish_batch_in_rollup(test_sequencer_client, &via_submit_txs).await?;
-        assert_eq!(
-            expected_total_hashes,
-            tx_hashes_a.len(),
-            "Wrong number total hashes returned from StandardSequencer for case: {:?}",
-            case
-        );
-        assert_eq!(tx_hashes_a, tx_hashes_b, "Number of hashes mismatch between StandardSequencer(left) and TestStatelessSequencer(right) for case {:?}", case);
-
+    let deferred_slots_count = config_deferred_slots_count();
+    let mut height_to_check = head + 1;
+    for i in 1..=deferred_slots_count {
         test_rollup.da_service.produce_block_now().await?;
-
-        // Wait for the slot to be processed, so rollup is in a good state.
-        let _slot = slots.next().await.unwrap()?;
-
-        // Compare submitted data
-        compare_block_at_height(head + 1 + idx as u64, &test_rollup.da_service).await;
+        let block = test_rollup.da_service.get_block_at(head + i).await?;
+        if !block.batch_blobs.is_empty() {
+            height_to_check = head + i;
+            trigger_publish_batch(test_sequencer_client).await?;
+            test_rollup.da_service.produce_block_now().await?;
+            break;
+        }
     }
+    // Wait for the slot to be processed, so rollup is in a good state.
+    let _slot = slots.next().await.unwrap()?;
+    compare_block_at_height(height_to_check, &test_rollup.da_service).await;
     Ok(())
 }
 
-fn generate_txs(user: &TestUser<TestSpec>, number: u64) -> Vec<RawTx> {
-    (1..=number)
-        .map(|i| {
-            let msg = TestRuntimeCall::Bank(
-                sov_test_utils::sov_bank::CallMessage::<TestSpec>::CreateToken {
-                    token_name: format!("sequencers-check-{}", i).try_into().unwrap(),
-                    initial_balance: 1000,
-                    mint_to_address: user.address(),
-                    admins: SafeVec::new(),
-                },
-            );
+fn generate_tx_with_nonce(user: &TestUser<TestSpec>, nonce: u64) -> RawTx {
+    let msg = TestRuntimeCall::Bank(
+        sov_test_utils::sov_bank::CallMessage::<TestSpec>::CreateToken {
+            token_name: format!("sequencers-check-{}", nonce).try_into().unwrap(),
+            initial_balance: 1000,
+            mint_to_address: user.address(),
+            admins: SafeVec::new(),
+        },
+    );
 
-            let tx = default_test_signed_transaction::<TestRuntime<TestSpec>, TestSpec>(
-                &user.private_key,
-                &msg,
-                i,
-                &TestRuntime::<TestSpec>::CHAIN_HASH,
-            );
+    let tx = default_test_signed_transaction::<TestRuntime<TestSpec>, TestSpec>(
+        &user.private_key,
+        &msg,
+        nonce,
+        &TestRuntime::<TestSpec>::CHAIN_HASH,
+    );
 
-            RawTx::new(borsh::to_vec(&tx).unwrap())
-        })
-        .collect()
+    RawTx::new(borsh::to_vec(&tx).unwrap())
 }
 
-async fn publish_batch_in_rollup(
-    api_client: &sov_api_spec::client::Client,
-    txs: &[RawTx],
-) -> anyhow::Result<Vec<TxHash>> {
+async fn trigger_publish_batch(api_client: &sov_api_spec::client::Client) -> anyhow::Result<()> {
     let request_body = sov_api_spec::types::PublishBatchBody {
-        transactions: txs
-            .iter()
-            .map(|tx| BASE64_STANDARD.encode(&tx.data))
-            .collect(),
+        transactions: vec![],
     };
-
     let response = api_client.publish_batch(&request_body).await?;
-
-    let receipt = response
+    let _receipt = response
         .data
         .clone()
         .ok_or_else(|| anyhow::anyhow!("Empty publish batch receipt data was empty"))?;
-
-    assert!(receipt.tx_hashes.len() >= txs.len());
-
-    Ok(receipt
-        .tx_hashes
-        .into_iter()
-        .map(|t| t.parse().unwrap())
-        .collect())
+    Ok(())
 }
 
 async fn accept_tx_in_rollup(
@@ -196,17 +148,39 @@ async fn accept_tx_in_rollup(
     tx_accepted.data.id.parse()
 }
 
-async fn compare_block_at_height(height: u64, da_service_1: &StorableMockDaService) {
-    let mut block = da_service_1
+async fn compare_block_at_height(height: u64, da_service: &StorableMockDaService) {
+    let block_1 = da_service
         .get_block_at(height)
         .await
         .expect("Failed to get block from DaService1");
-    assert_eq!(block.batch_blobs.len(), 2);
-    let raw_blobs: Vec<_> = block
+    assert_eq!(
+        block_1.batch_blobs.len(),
+        1,
+        "standard sequencer did not produce a batch"
+    );
+    let blob_from_std = get_single_blob(block_1).context("std sequencer").unwrap();
+    let block_2 = da_service.get_block_at(height + 1).await.unwrap();
+    let blob_from_stateless = get_single_blob(block_2)
+        .context("stateless sequencer")
+        .unwrap();
+    assert!(!blob_from_std.is_empty());
+    assert_eq!(
+        blob_from_std, blob_from_stateless,
+        "different blobs at height from sequencers"
+    );
+}
+
+fn get_single_blob(mut block: MockBlock) -> anyhow::Result<Vec<u8>> {
+    if block.batch_blobs.len() != 1 {
+        anyhow::bail!(
+            "Block does not have a single blob, but {}",
+            block.batch_blobs.len()
+        );
+    }
+    Ok(block
         .batch_blobs
         .iter_mut()
         .map(|b| b.full_data().to_vec())
-        .collect();
-    assert!(!raw_blobs[0].is_empty());
-    assert_eq!(raw_blobs[0], raw_blobs[1]);
+        .next()
+        .unwrap())
 }
