3a1f2fc8180e90bf339e180fc45c17f3e466e39b
Nikolai Golub <nikolai@sovlabs.io>
2025-06-20 15:19:16 +0200

    Mark `test_sequencer_rocksdb_db_performance_...` as flaky and increase timeout (#3071)
    * Bump timeout for `test_sequencer_rocksdb_db_performance`
    
    * More flaky tests
    
    * Timeout to be 90 seconds
    
    * Lint fix
    
    * Rollback marking as flaky other tests
    
    * Rollback CHANGELOG.md change again

--- crates/full-node/sov-sequencer/src/preferred/db/rocksdb.rs
+++ crates/full-node/sov-sequencer/src/preferred/db/rocksdb.rs
@@ -313,12 +313,12 @@ mod tests {
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn test_sequencer_rocksdb_db_performance_can_run_1k_batches_in_1_minute() {
+    async fn flaky_test_sequencer_rocksdb_db_performance_can_run_1k_batches_in_90_seconds() {
         let handle = tokio::task::spawn(run_rocksdb_test(10000));
 
-        let _ = tokio::time::timeout(std::time::Duration::from_secs(60), handle)
+        let _ = tokio::time::timeout(std::time::Duration::from_secs(90), handle)
             .await
-            .expect("Creating 10000 batches should take less than 1 minute; you may need to check your filesystem performance!");
+            .expect("Creating 10000 batches should take less than 90 seconds; you may need to check your filesystem performance!");
     }
 
     async fn run_rocksdb_test(iters: u64) {
