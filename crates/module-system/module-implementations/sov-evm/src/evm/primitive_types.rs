f1d73da25f630ab3512bae333fc311b534087ada
Blazej Kolad <blazejkolad@gmail.com>
2025-09-03 11:21:32 +0200

    Add support for transaction receipts from pending blocks (#1608)

--- crates/module-system/module-implementations/sov-evm/src/evm/primitive_types.rs
+++ crates/module-system/module-implementations/sov-evm/src/evm/primitive_types.rs
@@ -142,6 +142,61 @@ impl<'de> serde::Deserialize<'de> for SealedBlock {
     }
 }
 
+#[cfg(feature = "native")]
+pub(crate) enum MaybeSealedBlock {
+    Sealed(Box<SealedBlock>),
+    Pending {
+        block_number: u64,
+        first_tx_number: u64,
+        base_fee_per_gas: u64,
+    },
+}
+
+#[cfg(feature = "native")]
+impl MaybeSealedBlock {
+    pub fn hash(&self) -> Option<B256> {
+        match self {
+            Self::Sealed(block) => Some(block.header.hash()),
+            Self::Pending { .. } => None,
+        }
+    }
+
+    pub fn number(&self) -> u64 {
+        match self {
+            Self::Sealed(block) => block.header.number,
+            Self::Pending { block_number, .. } => *block_number,
+        }
+    }
+
+    pub fn transactions_start(&self) -> u64 {
+        match self {
+            Self::Sealed(block) => block.transactions.start,
+            Self::Pending {
+                first_tx_number, ..
+            } => *first_tx_number,
+        }
+    }
+
+    pub fn timestamp(&self) -> Option<u64> {
+        match self {
+            Self::Sealed(block) => Some(block.header.timestamp),
+            Self::Pending { .. } => None,
+        }
+    }
+
+    pub fn base_fee_per_gas(&self) -> u64 {
+        match self {
+            Self::Sealed(block) => block
+                .header
+                .base_fee_per_gas
+                .expect("Legacy blocks with no base fee are unsupported"),
+            Self::Pending {
+                base_fee_per_gas, ..
+            } => *base_fee_per_gas,
+        }
+    }
+}
+
 #[serde_as]
 #[derive(Debug, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Receipt {
