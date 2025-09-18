d917623e9c4500b63c22d3b66118880cf8aa649e
Blazej Kolad <blazejkolad@gmail.com>
2025-09-18 14:00:27 +0200

    EVM: `eth_getLogs` supprot for range queries (#1710)

--- crates/full-node/sov-ethereum/src/handlers/get_logs.rs
+++ crates/full-node/sov-ethereum/src/handlers/get_logs.rs
@@ -7,6 +7,7 @@ use crate::EthereumAuthenticator;
 use crate::FromVmAddress;
 use crate::HasKernel;
 use crate::Sequencer;
+use alloy_eips::BlockNumberOrTag;
 use alloy_primitives::B256;
 use alloy_rpc_types::eth::Filter;
 use alloy_rpc_types::FilterBlockOption;
@@ -14,8 +15,10 @@ use alloy_rpc_types::Log;
 use jsonrpsee::types::ErrorObjectOwned;
 use jsonrpsee::types::Params as JRpcParams;
 use jsonrpsee::Extensions;
+use sov_evm::PendingOrBlock;
 use sov_modules_api::ApiStateAccessor;
 use sov_modules_api::Spec;
+use std::ops::RangeInclusive;
 use std::sync::Arc;
 
 #[derive(Default, Debug, Clone, Copy)]
@@ -70,14 +73,9 @@ where
             logs_for_block_hash(filter, block_hash, state)
         }
         FilterBlockOption::Range {
-            from_block: _,
-            to_block: _,
-        } => {
-            return Err(to_jsonrpsee_error_object(
-                "FilterBlockOption::Range not supported",
-                ETH_RPC_ERROR,
-            ));
-        }
+            from_block,
+            to_block,
+        } => logs_for_blocks_range(filter, from_block, to_block, state),
     }
 }
 
@@ -98,6 +96,46 @@ where
         tracing::warn!(%msg);
         return Err(to_jsonrpsee_error_object(&msg, ETH_RPC_ERROR));
     };
+    logs_from_block(&mut rpc_logs, height, &filter, &evm, state)?;
+
+    Ok(rpc_logs)
+}
+
+fn logs_for_blocks_range<S>(
+    filter: Filter,
+    from_block: Option<BlockNumberOrTag>,
+    to_block: Option<BlockNumberOrTag>,
+    state: &mut ApiStateAccessor<S>,
+) -> Result<Vec<Log>, ErrorObjectOwned>
+where
+    S: Spec,
+    S::Address: FromVmAddress<EthereumAddress>,
+{
+    let mut rpc_logs = Vec::new();
+    let evm = sov_evm::Evm::<S>::default();
+
+    let start = get_block_nr(from_block, &evm, state)?;
+    let end = get_block_nr(to_block, &evm, state)?;
+
+    // TODO bloom filters
+    let block_range = RangeInclusive::new(start, end);
+    for height in block_range {
+        logs_from_block(&mut rpc_logs, height, &filter, &evm, state)?;
+    }
+    Ok(rpc_logs)
+}
+
+fn logs_from_block<S>(
+    rpc_logs: &mut Vec<Log>,
+    height: u64,
+    filter: &Filter,
+    evm: &sov_evm::Evm<S>,
+    state: &mut ApiStateAccessor<S>,
+) -> Result<(), ErrorObjectOwned>
+where
+    S: Spec,
+    S::Address: FromVmAddress<EthereumAddress>,
+{
     let block = evm.get_maybe_sealed_block(height, state);
 
     for index in block.transactions_start()..block.transactions_end() {
@@ -114,7 +152,7 @@ where
             if filter.matches(&log) {
                 let rpc_log = Log {
                     inner: log,
-                    block_hash: Some(block_hash),
+                    block_hash: block.hash(),
                     block_number: Some(receipt.block_number),
                     block_timestamp: Some(block.timestamp()),
                     transaction_hash: Some(receipt.transaction_hash),
@@ -127,5 +165,30 @@ where
         }
     }
 
-    Ok(rpc_logs)
+    Ok(())
+}
+
+fn get_block_nr<S>(
+    block_nr_or_tag: Option<BlockNumberOrTag>,
+    evm: &sov_evm::Evm<S>,
+    state: &mut ApiStateAccessor<S>,
+) -> Result<u64, ErrorObjectOwned>
+where
+    S: Spec,
+    S::Address: FromVmAddress<EthereumAddress>,
+{
+    let number = evm.str_to_block_nr(block_nr_or_tag.map(|b| b.to_string()), state);
+    match number {
+        PendingOrBlock::Pending => {
+            return Err(to_jsonrpsee_error_object(
+                "Pending blocks are not supported",
+                ETH_RPC_ERROR,
+            ))
+        }
+        PendingOrBlock::Invalid(err) => {
+            let msg = format!("Invalid block: {err}");
+            return Err(to_jsonrpsee_error_object(msg, ETH_RPC_ERROR));
+        }
+        PendingOrBlock::Number(number) => Ok(number),
+    }
 }
